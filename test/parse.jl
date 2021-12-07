fb07f0b38d6820c5dcf0fef1943022465c17cadd
Peter Ahrens <ptrahrens@gmail.com>
2021-12-07 15:03:13 -0500

    virtualization in 1 hour

---
+++ test/parse.jl
@@ -0,0 +1,19 @@
+using Finch: mesacall, mesaaccess, mesavalue, mesaname, mesaloop, mesawith, virtualize
+@testset "Parse" begin
+    @test @I(:f(:B[i, k] * :C[k, j]^3, 42)) ==
+    mesacall(mesavalue(:f), mesacall(mesavalue(*), mesaaccess(mesavalue(:B), Pigeon.Read(), mesaname(:i), mesaname(:k)), mesacall(mesavalue(^), mesaaccess(mesavalue(:C), Pigeon.Read(), mesaname(:k), mesaname(:j)), mesavalue(3))), mesavalue(42))
+
+    @test virtualize(:ex, typeof(@I(:f(:B[i, k] * :C[k, j]^3, 42)))) ==
+    call(:f, call(*, access(:B, Pigeon.Read(), Name(:i), Name(:k)), call(^, access(:C, Pigeon.Read(), Name(:k), Name(:j)), 3)), 42) 
+    call(:f, call(*, mesaaccess(:B, Pigeon.Read(), Name(:i), Name(:k)), call(^, mesaaccess(:C, Pigeon.Read(), Name(:k), Name(:j)), 3)), 42)
+    call(:f, call(*, access(:B, Pigeon.Read(), Name(:i), Name(:k)), call(^, access(:C, Pigeon.Read(), Name(:k), Name(:j)), 3)), 42)
+
+    #@test @i(
+    #    @loop i (
+    #        @loop j :A[i, j] += :w[j]
+    #    ) where (
+    #        @loop j k :w[j] += :B[i, k] * :C[k, j]
+    #    )
+    #) ==
+    #loop(Name(:i), with(loop(Name(:j), assign(access(:A, Pigeon.Update(), Name(:i), Name(:j)), +, access(:w, Pigeon.Read(), Name(:j)))), loop(Name(:j), Name(:k), assign(access(:w, Pigeon.Update(), Name(:j)), +, call(*, access(:B, Pigeon.Read(), Name(:i), Name(:k)), access(:C, Pigeon.Read(), Name(:k), Name(:j)))))))
+end
