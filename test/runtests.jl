133fc0f0ea5e2369a2e9093757e5e72feb37ea1d
Peter Ahrens <ptrahrens@gmail.com>
2021-12-17 15:08:57 -0500

    everything is a mess rn

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,6 +38,22 @@ end
         execute(ex)
         println(C)
     end
+
+    #=
+    @testset "simplerun plus vec" begin
+        println()
+        A = SimpleRunLength{Float64, Int, :A}([1, 3, 5, 7, 9, 10], [2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
+        B = ones(10)
+        println(A)
+        println(B)
+        C = SimpleRunLength{Float64, Int, :C}([1, 10], [0.0])
+        ex = @I @loop i C[i] += A[i] + B[i]
+        display(lower_julia(virtualize(:ex, typeof(ex))))
+        println()
+        execute(ex)
+        println(C)
+    end
+    =#
     exit()
 
     A = VirtualAbstractArray(1, :A, :A)
