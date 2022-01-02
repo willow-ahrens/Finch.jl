42bd4046e8676d65fa2b7a7c5386850e604044f8
Peter Ahrens <ptrahrens@gmail.com>
2022-01-02 17:09:15 -0500

    better tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,39 +19,10 @@ function Pigeon.visit!(node::Access{ChunkVector}, ctx::Finch.ChunkifyContext, ::
     return Access(node.tns.body, node.mode, node.idxs)
 end
 
+
 @testset "Finch.jl" begin
 
     include("parse.jl")
-    include("simplesparsetests.jl")
-    include("simplerunlengthtests.jl")
+    include("simplevectors.jl")
 
-    @testset "simplerun plus simplesparse" begin
-        println()
-        A = SimpleRunLength{Float64, Int}([1, 3, 5, 7, 9, 10], [2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
-        B = SimpleSparseVector{0.0, Float64, Int}([2, 5, 8, 11], [1.0, 1.0, 1.0])
-        println(A)
-        println(B)
-        C = SimpleRunLength{Float64, Int}([1, 10], [0.0])
-        ex = @I @loop i C[i] += A[i] + B[i]
-        println(typeof(ex))
-        display(lower_julia(virtualize(:ex, typeof(ex))))
-        println()
-        execute(ex)
-        println(C)
-    end
-
-    @testset "simplerun plus vec" begin
-        println()
-        A = SimpleRunLength{Float64, Int}([1, 3, 5, 7, 9, 10], [2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
-        B = ones(10)
-        println(A)
-        println(B)
-        C = SimpleRunLength{Float64, Int}([1, 10], [0.0])
-        ex = @I @loop i C[i] += A[i] + B[i]
-        println(typeof(ex))
-        display(lower_julia(virtualize(:ex, typeof(ex))))
-        println()
-        execute(ex)
-        println(C)
-    end
 end
---
+++ test/simplevectors.jl
@@ -0,0 +1,91 @@
+include("simplerunlength.jl")
+include("simplesparsevector.jl")
+
+@testset "simplevectors" begin
+    println("run = run + run")
+    A = SimpleRunLength{Float64, Int}([1, 3, 5, 7, 9, 10], [2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
+    B = SimpleRunLength{Float64, Int}([5, 8, 10], [1.0, 2.0, 3.0])
+    C = SimpleRunLength{Float64, Int}([1, 10], [0.0])
+    ex = @I @loop i C[i] += A[i] + B[i]
+
+    display(lower_julia(virtualize(:ex, typeof(ex))))
+    println()
+    execute(ex)
+
+    println(A)
+    println(B)
+    println(C)
+
+    @test C.idx == [1, 3, 5, 7, 8, 9, 10]
+    @test C.val == [3.0, 4.0, 5.0, 7.0, 8.0, 9.0, 10.0]
+    println()
+
+    println("dense = sparse + sparse")
+    A = SimpleSparseVector{0.0, Float64, Int}([1, 3, 5, 7, 9, 11], [2.0, 3.0, 4.0, 5.0, 6.0])
+    B = SimpleSparseVector{0.0, Float64, Int}([2, 5, 8, 11], [1.0, 1.0, 1.0])
+    C = zeros(10)
+    ex = @I @loop i C[i] += A[i] + B[i]
+
+    display(lower_julia(virtualize(:ex, typeof(ex))))
+    println()
+    execute(ex)
+
+    println(A)
+    println(B)
+    println(C)
+
+    @test C == [1.0, 1.0, 3.0, 0.0, 5.0, 0.0, 5.0, 1.0, 6.0, 0.0]
+    println()
+
+    println("sparse = sparse + sparse")
+    C = SimpleSparseVector{0.0, Float64, Int}([11], [])
+    ex = @I @loop i C[i] += A[i] + B[i]
+
+    display(lower_julia(virtualize(:ex, typeof(ex))))
+    println()
+    execute(ex)
+
+    println(A)
+    println(B)
+    println(C)
+
+    @test C.idx == [1, 2, 3, 5, 7, 8, 9, 11]
+    @test C.val == [2.0, 1.0, 3.0, 5.0, 5.0, 1.0, 6.0]
+    println()
+
+    println("run = run + sparse")
+    A = SimpleRunLength{Float64, Int}([5, 9, 10], [2.0, 6.0, 9.0])
+    B = SimpleSparseVector{0.0, Float64, Int}([3, 5, 8, 11], [1.0, 1.0, 1.0])
+    C = SimpleRunLength{Float64, Int}([1, 10], [0.0])
+    ex = @I @loop i C[i] += A[i] + B[i]
+
+    display(lower_julia(virtualize(:ex, typeof(ex))))
+    println()
+    execute(ex)
+
+    println(A)
+    println(B)
+    println(C)
+
+    @test C.idx == [2, 3, 4, 5, 7, 8, 9, 10]
+    @test C.val == [2.0, 3.0, 2.0, 3.0, 6.0, 7.0, 9.0]
+    println()
+
+    println("run = run + dense")
+    A = SimpleRunLength{Float64, Int}([1, 3, 5, 7, 9, 10], [2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
+    B = ones(10)
+    C = SimpleRunLength{Float64, Int}([1, 10], [0.0])
+    ex = @I @loop i C[i] += A[i] + B[i]
+
+    display(lower_julia(virtualize(:ex, typeof(ex))))
+    println()
+    execute(ex)
+
+    println(A)
+    println(B)
+    println(C)
+
+    @test C.idx == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    @test C.val == [3.0, 3.0, 4.0, 4.0, 5.0, 5.0, 6.0, 6.0, 7.0, 7.0]
+    println()
+end
