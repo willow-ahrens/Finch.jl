294f496ce3f55ab95a14da06b4d8911f6efe9ed4
Peter Ahrens <ptrahrens@gmail.com>
2022-01-04 18:34:21 -0500

    An interface

--- src/fibers.jl
+++ src/fibers.jl
@@ -4,61 +4,58 @@ export DenseLevel
 export DenseFiber
 export ScalarLevel
 
-struct FiberArray{Tv, N, Levels<:NTuple{N}} <: AbstractArray{Tv, N}
-    levels::Levels
+struct Fiber{Tv, N, R, Lvls<:Tuple, Poss<:Tuple, Idxs<:Tuple} <: AbstractArray{Tv, N}
+    lvls::Lvls
+    poss::Poss
+    idxs::Idxs
 end
 
-#represents a collection of SparseFibers
-struct SparseLevel{Tv, Ti, N} <: AbstractVector{Any} #should have fiber eltype
+Base.size(fbr::Fiber{Tv, N, R}) where {Tv, N, R} = map(dimension, fbr.lvls[R+1:end])
+
+function Base.getindex(fbr::Fiber{Tv, N}, idxs::Vararg{<:Any, N}) where {Tv, N}
+    readindex(fbr, idxs...)
+end
+
+function readindex(fbr::Fiber{Tv, N, R}, i, tail...) where {Tv, N, R}
+    unfurl(fbr.arr.lvl[R], fbr, i, idxs...)
+end
+
+function refurl(fbr::Fiber{Tv, N, R}, p, i) where {Tv, N, R}
+    return Fiber{Tv, N - 1, R + 1}(fbr.lvls, (fbr.poss..., p), (fbr.idxs..., i))
+end
+
+
+
+struct SparseLevel{Tv, Ti} <: AbstractVector{Any}
     Q::Ti
     I::Ti
     pos::Vector{Ti}
     idx::Vector{Ti}
-    child
 end
 
-dimension(lvl::SparseLevel) = (lvl.I, dimension(lvl.child)...)
-Base.size(lvl::SparseLevel) = lvl.Q
+dimension(lvl::SparseLevel) = lvl.I
+cardinality(lvl::SparseLevel) = lvl.Q
 
-struct SparseFiber{Tv, Ti, N} <: AbstractArray{Tv, N}
-    q::Ti
-    lvl::SparseLevel{Tv, Ti, N}
-end
-
-function Base.getindex(lvl::SparseLevel, q)
-    return SparseFiber(q, lvl)
-end
-
-Base.size(fbr::SparseFiber) = dimension(fbr.lvl)
-
-function Base.getindex(fbr::SparseFiber{Tv, Ti}, i, tail...) where {Tv, Ti}
-    r = searchsorted(@view(fbr.lvl.idx[fbr.lvl.pos[fbr.q]:fbr.lvl.pos[fbr.q + 1] - 1]), i)
-    length(r) == 0 ? zero(Tv) : fbr.lvl.child[fbr.lvl.pos[fbr.q] + first(r) - 1][tail...]
+function unfurl(lvl::SparseLevel{Tv, Ti}, fbr::Fiber{Tv, N, R}, i, tail...) where {Tv, Ti, N, R}
+    q = R == 0 ? 1 : fbr.poss[R]
+    r = searchsorted(@view(lvl.idx[lvl.pos[q]:lvl.pos[q + 1] - 1]), i)
+    p = fbr.lvl.pos[fbr.q] + first(r) - 1
+    length(r) == 0 ? zero(Tv) : readindex(refurl(fbr, p, i), tail...)
 end
 
 #represents a collection of DenseFibers
 struct DenseLevel{Tv, Ti, N} <: AbstractVector{Any} #should have fiber eltype
     Q::Ti
     I::Ti
-    child
 end
 
-dimension(lvl::DenseLevel) = (lvl.I, dimension(lvl.child)...)
-Base.size(lvl::DenseLevel) = lvl.Q
+dimension(lvl::DenseLevel) = lvl.I
+cardinality(lvl::DenseLevel) = lvl.Q
 
-struct DenseFiber{Tv, Ti, N} <: AbstractArray{Tv, N}
-    q::Ti
-    lvl::DenseLevel{Tv, Ti, N}
-end
-
-function Base.getindex(lvl::DenseLevel, q)
-    return DenseFiber(q, lvl)
-end
-
-Base.size(fbr::DenseFiber) = dimension(fbr.lvl)
-
-function Base.getindex(fbr::DenseFiber, i, tail...)
-    fbr.lvl.child[(fbr.q - 1) * fbr.lvl.I + i][tail...]
+function unfurl(lvl::DenseLevel{Tv, Ti}, fbr::Fiber{Tv, N, R}, i, tail...) where {Tv, Ti, N, R}
+    q = R == 0 ? 1 : fbr.poss[R]
+    p = (q - 1) * lvl.I + i
+    readindex(refurl(fbr, p, i), tail...)
 end
 
 #represents scalars
@@ -67,21 +64,9 @@ struct ScalarLevel{Tv, V <: AbstractVector{Tv}} <: AbstractVector{Tv}
 end
 
 dimension(lvl::ScalarLevel) = ()
-Base.size(lvl::ScalarLevel) = size(lvl.val)
+cardinality(lvl::ScalarLevel) = size(lvl.val)
 
-function Base.getindex(lvl::ScalarLevel, q)
+function unfurl(lvl::ScalarLevel, fbr::Fiber{Tv, N, R}) where {Tv, N, R}
+    q = R == 0 ? 1 : fbr.poss[R]
     return lvl.val[q]
-end
-
-struct ScalarFiber{Tv, Ti, V} <: AbstractArray{Tv, 0}
-    q::Ti
-    lvl::ScalarLevel{Tv, V}
-end
-
-function Base.getindex(fbr::ScalarFiber)
-    fbr.lvl.val[fbr.q]
-end
-
-function lower_access(node::Virtual{T}, ctx) where {T <: ScalarFiber}
-    return :($(node.expr).lvl.val[$(node.expr).q])
 end
