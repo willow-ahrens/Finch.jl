1597bcb0c8ff3bd88caf4250dc25c50d2085ba77
Peter Ahrens <ptrahrens@gmail.com>
2022-04-28 14:02:17 -0400

    make sure we work with defaults

--- src/shifts.jl
+++ src/shifts.jl
@@ -10,3 +10,7 @@ function make_style(root, ctx::LowerJulia, node::Shift)
     style = make_style(root, ctx, node.body)
     @assert supports_shift(style)
 end
+function (ctx::ForLoopVisitor)(node::Shift, ::DefaultStyle)
+    ctx_2 = ForLoopVisitor(ctx, idx, Call(+, val, node.shift))
+    ctx_2(node.body)
+end
