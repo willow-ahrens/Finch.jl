9e4d03ecb8a86184953232f61c985f6050d3f828
Peter Ahrens <ptrahrens@gmail.com>
2022-05-02 16:26:04 -0400

    more parse tests

--- test/parse.jl
+++ test/parse.jl
@@ -1,5 +1,5 @@
 using Finch.IndexNotation
-using Finch.IndexNotation: call_instance, access_instance, value_instance, name_instance, loop_instance, with_instance, label_instance, walk_instance
+using Finch.IndexNotation: call_instance, assign_instance, access_instance, value_instance, name_instance, loop_instance, with_instance, label_instance, walk_instance
 
 @testset "Parse" begin
     @test @index_program_instance(:f(:B[i::walk, k] * :C[k, j]^3, 42)) ==
@@ -10,6 +10,12 @@ using Finch.IndexNotation: call_instance, access_instance, value_instance, name_
 
     @test Finch.virtualize(:ex, typeof(@index_program_instance((:A[] = 1; :B[] = 2))), Finch.LowerJulia()) ==
     multi(assign(access(:A, Read()), 1), assign(access(:B, Read()), 2))
+
+    @test @index_program(@loop i :A[i] += :B[i] * i) ==
+    loop(Name(:i), assign(access(:A, Update(), Name(:i)), +, call(*, access(:B, Read(), Name(:i)), Name(:i))))
+
+    @test @index_program_instance(@loop i :A[i] += :B[i] * i) ==
+    loop_instance(name_instance(:i), assign_instance(access_instance(value_instance(:A), Update(), name_instance(:i)), label_instance(:+, value_instance(+)), call_instance(label_instance(:*, value_instance(*)), access_instance(value_instance(:B), Read(), name_instance(:i)), name_instance(:i))))
 
     #call(:f, call(*, access_instance(:B, Read(), Name(:i), Name(:k)), call(^, access_instance(:C, Read(), Name(:k), Name(:j)), 3)), 42)
     #call(:f, call(*, access(:B, Read(), Name(:i), Name(:k)), call(^, access(:C, Read(), Name(:k), Name(:j)), 3)), 42)
