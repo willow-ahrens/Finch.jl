bd4a32b1d4cde2f4e7660ac12d0928e515bc706c
Peter Ahrens <ptrahrens@gmail.com>
2022-05-31 13:36:57 -0400

    cool

--- src/virtualize.jl
+++ src/virtualize.jl
@@ -26,10 +26,10 @@ function virtualize(ex, ::Type{IndexNotation.MultiInstance{Bodies}}, ctx) where 
     end
     Multi(bodies)
 end
-function virtualize(ex, ::Type{IndexNotation.SkipInstance{Cond, Body}}, ctx) where {Cond, Body}
+function virtualize(ex, ::Type{IndexNotation.SieveInstance{Cond, Body}}, ctx) where {Cond, Body}
     cond = virtualize(:($ex.cond), Cond, ctx)
     body = virtualize(:($ex.body), Body, ctx)
-    Skip(cond, body)
+    Sieve(cond, body)
 end
 function virtualize(ex, ::Type{IndexNotation.LoopInstance{Idx, Body}}, ctx) where {Idx, Body}
     idx = virtualize(:($ex.idx), Idx, ctx)
