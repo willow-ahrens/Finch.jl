1dc95f4bc4bf69c8ace9b29fd837f60678c5c1d3
Peter Ahrens <ptrahrens@gmail.com>
2022-06-17 18:57:21 -0400

    first draft

---
+++ src/permit.jl
@@ -0,0 +1,43 @@
+struct Permit{T}
+    I::T
+end
+
+const permit = Permit(nodim)
+
+Base.size(vec::Permit) = stop(vec.I) - start(vec.I) + 1
+
+function Base.getindex(vec::Permit, i)
+    start(vec.I) <= i <= stop(vec.I) ? i : missing
+end
+
+struct VirtualPermit
+    I
+end
+
+isliteral(::VirtualPermit) = false
+
+function virtualize(ex, ::Type{Permit{T}}, ctx) where {T}
+    return VirtualPermit(virtualize(:($ex.I), T, ctx))
+end
+
+function (ctx::Finch.LowerJulia)(tns::VirtualPermit)
+    quote
+        Permit($(ctx(tns.I)))
+    end
+end
+
+function Finch.getdims(arr::VirtualPermit, ctx::Finch.LowerJulia, mode)
+    return arr.I
+end
+Finch.setdims!(arr::VirtualPermit, ctx::Finch.LowerJulia, mode, dim) = VirtualPermit(dim)
+
+function (ctx::InferDimensions)(node::Access{VirtualPermit}, ext)
+    @assert length(node.idxs) == 1
+    ctx(node.idx, ext)
+    return ext
+end
+
+(ctx::InferDimensions)(node::Protocol, ext) = ctx(node.idx, ext)
+
+Finch.make_style(root::Loop, ctx::Finch.LowerJulia, node::Access{<:Permit}) =
+    getname(root.idx) == getname(node.idxs[1]) ? Finch.ChunkStyle() : Finch.DefaultStyle()
