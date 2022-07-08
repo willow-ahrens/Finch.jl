b8024850ecb875580e04416cd7ce467863953611
Willow Ahrens <willow.ahrens@mit.edu>
2022-07-08 18:02:58 -0400

    hmm.

--- test/test_permit.jl
+++ test/test_permit.jl
@@ -22,12 +22,13 @@
         Element{0.0}([1, 1])))
     C = Finch.Fiber(Solid(Element{0.0}()))
 
-    println(@index_code_lowered @loop i C[i] = coalesce(A[permit[i]], B[offset[5, permit[i]]]))
+    #println(@index_code_lowered @loop i C[i] = coalesce(A[permit[i]], B[offset[5, permit[i]]]))
     #println(@index_code_lowered @loop i C[i] = coalesce(A[permit[i]], B[permit[offset[5, i]]]))
-    @index @loop i C[i] = coalesce(A[permit[i]], B[offset[5, permit[i]]])
+    #@index @loop i C[i] = coalesce(A[permit[i]], B[offset[5, permit[i]]])
     #@index @loop i C[i] = coalesce(A[permit[i]], B[permit[offset[5, i]]])
     #println(FiberArray(C))
-    #println(@index_code_lowered @loop j i C[i] += B[j] * coalesce(A[offset[j - 1, permit[i]]], 0))
+    println(@index_code_lowered @loop j i C[i] += B[j] * coalesce(A[offset[j - 1, permit[i]]], 0))
+    println(@index_code_lowered @loop i j C[i] += B[j] * coalesce(A[offset[i - 1, permit[j]]], 0))
     println(FiberArray(C))
 
 end
