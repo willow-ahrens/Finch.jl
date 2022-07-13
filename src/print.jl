a7ebc1b21c6515a871f42540f0b043c0236ae314
Peter Ahrens <ptrahrens@gmail.com>
2022-07-13 13:23:36 -0400

    hmm

--- src/print.jl
+++
@@ -1,7 +0,0 @@
-using Finch
-using SparseArrays
-display(fiber(sprand(100, 0.5)))
-display(fiber(sprand(100, 100, 0.5)))
-display(fiber(rand(100, 100)))
-display(fiber(rand(100, 100)))
-display(fiber(rand(10, 10, 10)))
