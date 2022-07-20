1cf380a11ff40fb04151160fe7b416aa29e9c3f3
Willow Ahrens <willow.ahrens@mit.edu>
2022-07-20 11:47:53 -0400

    it lives!

--- demos/interactive_finch.jl
+++ demos/interactive_finch.jl
@@ -17,8 +17,8 @@ C = similar(A)
 
 F = fiber([1, 1, 1, 1, 1])
 
-@index_code @loop i j C[i] += (A[i] != 0) * coalesce(A[permit[offset[i - 3, j]]], 0) * F[j]
-@index @loop i j C[i] += (A[i] != 0) * coalesce(A[permit[offset[i - 3, j]]], 0) * F[j]
+@index @loop i j C[i] += (A[i] != 0) * coalesce(A[permit[offset[3-i, j]]], 0) * coalesce(F[permit[j]], 0)
+@index_code @loop i j C[i] += (A[i] != 0) * coalesce(A[permit[offset[3-i, j]]], 0) * coalesce(F[permit[j]], 0)
 
 @index_code @loop i C[i] = A[i] + B[i]
 @index @loop i C[i] = A[i] + B[i]
