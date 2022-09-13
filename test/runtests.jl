8c1eecdf79911ff93bd5d0eb8faf6ef745febe57
Peter Ahrens <ptrahrens@gmail.com>
2022-09-13 08:22:43 -0400

    return boolean

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,35 +6,34 @@ include("data_matrices.jl")
 
 function diff(name, body)
     global ARGS
-    if "diff" in ARGS
-        cache_dir = mkpath(joinpath(@__DIR__, "cache"))
-        temp_dir = mkpath(joinpath(@__DIR__, "temp"))
-        cache_file = joinpath(cache_dir, name)
-        temp_file = joinpath(temp_dir, name)
-        open(temp_file, "w") do f
+    "diff" in ARGS || return true
+    cache_dir = mkpath(joinpath(@__DIR__, "cache"))
+    temp_dir = mkpath(joinpath(@__DIR__, "temp"))
+    cache_file = joinpath(cache_dir, name)
+    temp_file = joinpath(temp_dir, name)
+    open(temp_file, "w") do f
+        println(f, body)
+    end
+    if "overwrite" in ARGS
+        open(cache_file, "w") do f
             println(f, body)
         end
-        if "overwrite" in ARGS
-            open(cache_file, "w") do f
-                println(f, body)
-            end
-            true
+        true
+    else
+        if success(`diff --strip-trailing-cr $cache_file $temp_file`)
+            return true
         else
-            if success(`diff --strip-trailing-cr $cache_file $temp_file`)
-                return true
-            else
-                if "verbose" in ARGS
-                    println("=== reference ===")
-                    open(cache_file, "r") do f
-                        for line in eachline(f)
-                            println(line)
-                        end
+            if "verbose" in ARGS
+                println("=== reference ===")
+                open(cache_file, "r") do f
+                    for line in eachline(f)
+                        println(line)
                     end
-                    println("=== test ===")
-                    println(body)
                 end
-                return false
+                println("=== test ===")
+                println(body)
             end
+            return false
         end
     end
 end
