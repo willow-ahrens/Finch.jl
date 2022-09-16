8576ba3a75508c40df855d9db902bb55d60119d6
Willow Ahrens <willow.ahrens@mit.edu>
2022-09-16 17:53:55 -0400

    hmm

--- test/time_to_first_spmspv.jl
+++ test/time_to_first_spmspv.jl
@@ -6,3 +6,4 @@ println(@elapsed Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @l
 # With no precompilation or anything, this takes 71.272644599s on my garbage macbookpro
 # With precompilation, this takes 48.013980111s on my garbage macbookpro
 # With precompilation and removing excess style computations, this takes 45.323332916s on my garbage macbookpro
+# After making arrays more reliably Any, this takes 44.811293213
