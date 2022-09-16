8a8dbdf5d0c3878d6d655dd186a72d6c4f4dfc42
Willow Ahrens <willow.ahrens@mit.edu>
2022-09-16 19:23:02 -0400

    hmm.

--- test/time_to_first_spmspv.jl
+++ test/time_to_first_spmspv.jl
@@ -1,9 +1,13 @@
 using Finch
+using Profile
+using ProfileView
 y = @fiber d(e(0.0))
 A = @fiber d(sl(e(0.0)))
 x = @fiber sl(e(0.0))
+display(@profview Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @loop i j y[i] += A[i, j] * x[i])))
 println(@elapsed Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @loop i j y[i] += A[i, j] * x[i])))
 # With no precompilation or anything, this takes 71.272644599s on my garbage macbookpro
 # With precompilation, this takes 48.013980111s on my garbage macbookpro
 # With precompilation and removing excess style computations, this takes 45.323332916s on my garbage macbookpro
 # After making arrays more reliably Any, this takes 44.811293213, 7m09.5s to run tests
+# If we never call RewriteTools, this would take 24.325700075s
