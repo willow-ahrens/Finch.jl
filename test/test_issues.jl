7d45f1d37e18b70a36820ebbc32e1471aee12ede
Willow Ahrens <willow.ahrens@mit.edu>
2022-09-20 12:26:26 -0400

    Fixes #53

--- test/test_issues.jl
+++ test/test_issues.jl
@@ -8,4 +8,15 @@
         @test y[] == 14
     end
 
+    #https://github.com/willow-ahrens/Finch.jl/issues/53
+    let
+        x = @fiber sl(10, [1, 5], [1, 3, 7, 8], p())
+        y = Scalar{0.0}()
+        @finch @loop i y[] += ifelse(x[i], 3, -1)
+        @test y[] == 6
+        a = 3
+        b = -1
+        @test diff("ifelse53.jl", @finch_code @loop i y[] += ifelse(x[i], $a, $b))
+    end
+
 end
