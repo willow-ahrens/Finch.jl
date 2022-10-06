1316392103769d37826f743f8a6dfccac8680e28
Alexandra Dima <adadima@mit.edu>
2022-10-06 12:06:46 -0400

    Weird way of separating julia compile time, but not finch

--- demos/pr.c
+++ demos/pr.c
@@ -20,74 +20,89 @@ double beta_score;
 jl_value_t* edges = 0;
 jl_value_t* out_degree = 0;
 
+jl_function_t* funcode;
+
+jl_function_t* out_deg_code;
+jl_function_t* out_deg_ex;
+
+jl_function_t* rinit_code;
+jl_function_t* rinit_ex;
+
+jl_function_t* r_code;
+jl_function_t* r_ex;
+
+jl_function_t* rank_code;
+jl_function_t* rank_ex;
+
 struct pr_data {
-    jl_value_t* contrib;
 
     jl_value_t* ranks;
 
     jl_value_t* r;
 };
 
+// jl_value_t* call_finch(jl_function_t* b, va_list args)
+// {
+//     printf("in Call helper, func to call: %p\n", b);
+//     return finch_call(b, args);
+// }
+
+#define call_finch(f, ...) finch_call(f, ##__VA_ARGS__)
+
+#define run_finch(codegen, exgen, funcode, ...) {\
+    jl_value_t* code = finch_call(codegen, ##__VA_ARGS__); \
+    jl_value_t* ex = finch_call(exgen, ##__VA_ARGS__); \
+    finch_call(funcode, code, ex);\
+}
+
+// jl_value_t* run_finch(jl_function_t* codegen, jl_function_t* exgen, jl_function_t* funcode, ...) {
+//     va_list args;
+//     va_start(args, funcode);
+//     printf("Getting codegen\n");
+//     jl_value_t* code = call_finch(codegen, args);
+//     va_end(args);
+
+//     va_list args_;
+//     va_start(args_, funcode);
+//     printf("Getting ex\n");
+//     jl_value_t* ex = call_finch(exgen, args_);
+//     va_end(args_);
+    
+//     return finch_call(funcode, code, ex);
+// }
+
 // Let InitRank() -> (r_out float[N])
 //     out_d[j] = edges[i][j] | i:(+, 0)
 //     r_out[j] = 1.0 / N
 // End
 void Init(struct pr_data* data) {
-    jl_function_t* out_d_init = finch_eval("function out_d_init(out_d, edges)\n\
-    @finch @loop i j out_d[j] += edges[i, j]\n\
-end");
-
     out_degree = finch_Fiber(
         finch_Dense(finch_Cint(N),
         finch_ElementLevel(finch_Cint(0), finch_eval("Cint[]")))
     );
-    finch_call(out_d_init, out_degree, edges);
+    run_finch(out_deg_code, out_deg_ex, funcode, out_degree, edges);
     printf("Out degree: \n");
-    finch_exec("println(%s.lvl.lvl.val)", out_degree);
+    // finch_exec("println(%s.lvl.lvl.val)", out_degree);
 
     jl_value_t* r = finch_Fiber(
         finch_Dense(finch_Cint(N),
         finch_ElementLevel(finch_Float64(0), finch_eval("Float64[]")))
     );
-    
-    jl_function_t* r_init = finch_eval("function r_init(r, N)\n\
-    @finch @loop j r[j] = 1.0 / $N\n\
-end");
-    printf("function pointer: %p\n", r_init);
-    finch_call(r_init, r, finch_Cint(N));
-
+    run_finch(rinit_code, rinit_ex, funcode, r, finch_Cint(N));
     printf("r: \n");
-    finch_exec("println(%s.lvl.lvl.val)", r);
+    // finch_exec("println(%s.lvl.lvl.val)", r);
 
     data->r = r;
 }
 
-void Contrib_Update(struct pr_data* in_data, struct pr_data* out_data) {
-    jl_function_t* contrib_func = finch_eval("function c_init(contrib, r_in, out_d)\n\
-    @finch @loop i contrib[i] = r_in[i] / out_d[i]\n\
-end");
-    jl_value_t* contrib = finch_Fiber(
-        finch_Dense(finch_Cint(N),
-        finch_ElementLevel(finch_Float64(0), finch_eval("Float64[]")))
-    );
-    finch_call(contrib_func, contrib, in_data->r, out_degree);
-    printf("Contrib: \n");
-    finch_exec("println(%s.lvl.lvl.val)", contrib);
-
-    out_data->contrib = contrib;
-}
-
 void Rank_Update(struct pr_data* in_data, struct pr_data* out_data) {
-    jl_function_t* rank_func = finch_eval("function rank_func(rank, edges, r_in, out_d)\n\
-    @finch @loop i j rank[i] += ifelse(out_d[j] != 0, edges[i, j] * r_in[j] / out_d[j], 0)\n\
-end");
     jl_value_t* rank = finch_Fiber(
         finch_Dense(finch_Cint(N),
         finch_ElementLevel(finch_Float64(0), finch_eval("Float64[]")))
     );
-    finch_call(rank_func, rank, edges, in_data->r, out_degree);
-    printf("Rank: \n");
-    finch_exec("println(%s.lvl.lvl.val)", rank);
+    run_finch(rank_code, rank_ex, funcode,  rank, edges, in_data->r, out_degree);
+    printf("New Rank: \n");
+    // finch_exec("println(%s.lvl.lvl.val)", rank);
 
     out_data->ranks = rank;
     
@@ -101,17 +116,13 @@ end");
 }
 
 void R_Update(struct pr_data* out_data) {
-    jl_function_t* r_func = finch_eval("function r_func(r_out, beta_score, damp, rank)\n\
-    @finch @loop i r_out[i] = $beta_score + $damp * rank[i]\n\
-end");
     jl_value_t* r_out = finch_Fiber(
         finch_Dense(finch_Cint(N),
         finch_ElementLevel(finch_Float64(0), finch_eval("Float64[]")))
     );
-    finch_call(r_func, r_out, finch_Float64(beta_score), finch_Float64(damp), out_data->ranks);
-
-    printf("R: \n");
-    finch_exec("println(%s.lvl.lvl.val)", r_out);
+    run_finch(r_code, r_ex, funcode, r_out, finch_Float64(beta_score), finch_Float64(damp), out_data->ranks);
+    printf("New R: \n");
+    // finch_exec("println(%s.lvl.lvl.val)", r_out);
 
     out_data->r = r_out;
 }
@@ -126,10 +137,6 @@ void PageRankStep(struct pr_data* in_data, struct pr_data* out_data) {
     R_Update(out_data);
 }
 
-int outer_loop_condition(jl_value_t* F) {
-
-}
-
 // Let PageRank() -> (contrib float[N], rank float[N], r_out float[N])
 //     contrib[i] = 0.0
 //     rank[i] = 0.0
@@ -142,20 +149,94 @@ void PageRank(struct pr_data* data) {
 
     for(int i=0; i < 20; i++) {
         PageRankStep(data, &new_data);
-        *data = new_data;
+        printf("iteration %d DONE\n", i);
+        finch_free(data->r);
+        finch_free(data->ranks);
+        data->r = new_data.r;
+        data->ranks = new_data.ranks;
     }
+}
+
+
+void compile_finch() {
+    funcode = finch_eval("function call(code, ex_)\n\
+        fun = eval(Expr(:->,:ex, code))\n\
+        Base.invokelatest(fun, ex_)\n\
+    end");
+
+    out_deg_code = finch_eval("println(\"HELLO\")\n\
+    function outdeg_codegen(out_d, edges)\n\
+            code = @finch_code @loop i j out_d[j] += edges[i, j]\n\
+            return code\n\
+    end");
+    out_deg_ex = finch_eval("function outdeg_ex(out_d, edges)\n\
+            ex = @finch_program_instance @loop i j out_d[j] += edges[i, j]\n\
+            return ex\n\
+    end");
+    
+    rinit_code = finch_eval("function rinit_codegen(r, N)\n\
+        code = @finch_code @loop j r[j] = 1.0 / $N\n\
+        return code\n\
+    end");  
+    rinit_ex = finch_eval("function rinit_ex(r, N)\n\
+        ex = @finch_program_instance @loop j r[j] = 1.0 / $N\n\
+        return ex\n\
+    end");
+
+    rank_code = finch_eval("function rank_codegen(rank, edges, r_in, out_d)\n\
+        code = @finch_code @loop j i rank[j] += ifelse(out_d[i] != 0, edges[j, i] * r_in[i] / out_d[i], 0)\n\
+        return code\n\
+    end");
+    rank_ex = finch_eval("function rank_ex(rank, edges, r_in, out_d)\n\
+        ex = @finch_program_instance @loop j i rank[j] += ifelse(out_d[i] != 0, edges[j, i] * r_in[i] / out_d[i], 0)\n\
+        return ex\n\
+    end");
+
+    r_code = finch_eval("function r_codegen(r_out, beta_score, damp, rank)\n\
+        code = @finch_code @loop i r_out[i] = $beta_score + $damp * rank[i]\n\
+        return code\n\
+    end");
+    r_ex = finch_eval("function r_ex(r_out, beta_score, damp, rank)\n\
+        ex = @finch_program_instance @loop i r_out[i] = $beta_score + $damp * rank[i]\n\
+        return ex\n\
+    end");
+}
+
+
+void make_weights_and_edges(const char* graph_name, int n) {
+    // 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+    char code[1000];
+    sprintf(code, "N = %d\n\
+        matrix = copy(transpose(MatrixMarket.mmread(\"./graphs/%s\")))\n\
+        nzval = ones(size(matrix.nzval, 1))\n\
+        Finch.Fiber(\n\
+                 Dense(N,\n\
+                 SparseList(N, matrix.colptr, matrix.rowval,\n\
+                 Element{0}(nzval))))", n, graph_name);
+    edges = finch_eval(code);
+    printf("Loaded edges\n");
+}
+
+void starter() {
+    N = 1;
+
+    make_weights_and_edges("starter.mtx", N);
+
+    struct pr_data d = {};
+    struct pr_data* data = &d;
+    PageRank(data);
+
+    printf("EXAMPLE\nFinal: \n");
+    finch_exec("println(%s.lvl.lvl.val)", data->r);
 }
 
 void setup1() {
     // 1 5, 4 5, 3 4, 2 3, 1 2
     // jl_value_t* edge_vector = finch_eval("Cint[0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]");
     N = 5;
-    edges = finch_eval("N = 5\n\
-        edge_matrix = sparse([0 0 0 0 0; 1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 1 0 0 1 0])\n\
-        Finch.Fiber(\n\
-                 Dense(N,\n\
-                 SparseList(N, edge_matrix.colptr, edge_matrix.rowval,\n\
-                 Element{0.0}(edge_matrix.nzval))))");
+
+    make_weights_and_edges("dag5.mtx", N);
+
     struct pr_data d = {};
     struct pr_data* data = &d;
     PageRank(data);
@@ -167,12 +248,9 @@ void setup1() {
 void setup2() {
     // 2 1, 3 1, 3 2, 3 4
     N = 4;
-    edges = finch_eval("N = 4\n\
-        edge_matrix = sparse([0 1 1 0; 0 0 1 0; 0 0 0 0; 0 0 1 0])\n\
-        Finch.Fiber(\n\
-                 Dense(N,\n\
-                 SparseList(N, edge_matrix.colptr, edge_matrix.rowval,\n\
-                 Element{0.0}(edge_matrix.nzval))))");
+
+    make_weights_and_edges("dag4.mtx", N);
+
     struct pr_data d = {};
     struct pr_data* data = &d;
     PageRank(data);
@@ -185,12 +263,9 @@ void setup3() {
     // 2 1, 3 1, 1 2, 3 2, 1 3
     // jl_value_t* edge_vector = finch_eval("Cint[0, 1, 1, 1, 0, 0, 1, 1, 0]");
     N = 3;
-    edges = finch_eval("N = 3\n\
-        edge_matrix = sparse([0 1 1; 1 0 1; 1 0 0])\n\
-        Finch.Fiber(\n\
-                 Dense(N,\n\
-                 SparseList(N, edge_matrix.colptr, edge_matrix.rowval,\n\
-                 Element{0.0}(edge_matrix.nzval))))");
+    
+    make_weights_and_edges("dag3.mtx", N);
+
     struct pr_data d = {};
     struct pr_data* data = &d;
     PageRank(data);
@@ -203,17 +278,27 @@ void setup4() {
     // 3 2, 3 1, 4 3, 5 4, 6 5, 6 7, 7 5, 7 6
     // jl_value_t* edge_vector = finch_eval("Cint[0,0,0,0,0,0,0, 0,0,1,0,0,0,0, 1,0,0,0,0,0,0, 0,0,1,0,0,0,0, 0,0,0,1,0,0,0, 0,0,0,0,1,0,1, 0,0,0,0,1,1,0]");
     N = 7;
-    edges = finch_eval("N = 7\n\
-    edge_matrix = sparse([0 0 1 0 0 0 0; 0 0 0 0 0 0 0; 0 1 0 1 0 0 0; 0 0 0 0 1 0 0; 0 0 0 0 0 1 1; 0 0 0 0 0 0 1; 0 0 0 0 0 1 0])\n\
-    Finch.Fiber(\n\
-                Dense(N,\n\
-                SparseList(N, edge_matrix.colptr, edge_matrix.rowval,\n\
-                Element{0.0}(edge_matrix.nzval))))");
+    
+    make_weights_and_edges("dag7.mtx", N);
+
     struct pr_data d = {};
     struct pr_data* data = &d;
     PageRank(data);
 
     printf("EXAMPLE4\n Final: \n");
+    finch_exec("println(%s.lvl.lvl.val)", data->r);
+}
+
+void setup5() {
+    N = 4847571;
+    
+    make_weights_and_edges("soc-LiveJournal1.mtx", N);
+
+    struct pr_data d = {};
+    struct pr_data* data = &d;
+    PageRank(data);
+
+    printf("LARGE GRAPH\n Final: \n");
     finch_exec("println(%s.lvl.lvl.val)", data->r);
 }
 
@@ -224,6 +309,7 @@ int main(int argc, char** argv) {
     jl_value_t* res = finch_eval("using RewriteTools\n\
     using Finch.IndexNotation\n\
     using SparseArrays\n\
+     using MatrixMarket\n\
     ");
 
     res = finch_eval("or(x,y) = x == 1|| y == 1\n\
@@ -275,13 +361,24 @@ Finch.register()");
     damp = 0.85;
     beta_score = (1.0 - damp) / N;
 
+    N = 1;
+
+    // compile all the necessary functions first
+    compile_finch();
+    printf("Compiled finch\n");
+
+    starter();
+    printf("Ran starter\n");
+
     setup1();
 
-    setup2();
+    // setup2();
 
-    setup3();
+    // setup3();
 
-    setup4();
+    // setup4();
+
+    // setup5();
 
     finch_finalize();
 }
