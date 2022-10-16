c2e2d723da36b3d3a8fe1c287a484a2a1f222ae9
Peter Ahrens <ptrahrens@gmail.com>
2022-10-15 22:23:00 -0400

    add compile time benchmarks

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,63 @@
+using Pkg
+tempdir = mktempdir()
+Pkg.activate(tempdir)
+Pkg.develop(PackageSpec(path = joinpath(@__DIR__, "..")))
+Pkg.add(["BenchmarkTools", "PkgBenchmark", "MatrixDepot"])
+Pkg.resolve()
+
+using Finch
+using BenchmarkTools
+
+const SUITE = BenchmarkGroup()
+
+SUITE["compile"] = BenchmarkGroup()
+
+code = """
+using Finch
+y = @fiber d(e(0.0))
+A = @fiber d(sl(e(0.0)))
+x = @fiber sl(e(0.0))
+
+@finch @loop i j y[i] += A[i, j] * x[i]
+"""
+cmd = pipeline(`$(Base.julia_cmd()) --project=$(Base.active_project()) --eval $code`, stdout = IOBuffer())
+
+SUITE["compile"]["time_to_first_SpMV"] = @benchmarkable run($cmd)
+
+let
+    y = @fiber d(e(0.0))
+    A = @fiber d(sl(e(0.0)))
+    x = @fiber sl(e(0.0))
+
+    Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @loop i j y[i] += A[i, j] * x[i]))
+
+    SUITE["compile"]["compile_SpMV"] = @benchmarkable begin
+        y, A, x = ($y, $A, $x)
+        Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @loop i j y[i] += A[i, j] * x[i]))
+    end
+end
+
+code = """
+using Finch
+A = @fiber d(sl(e(0.0)))
+B = @fiber d(sl(e(0.0)))
+C = @fiber d(sl(e(0.0)))
+
+@finch @loop i j k C[i, j] += A[i, k] * B[j, k]
+"""
+cmd = pipeline(`$(Base.julia_cmd()) --project=$(Base.active_project()) --eval $code`, stdout = IOBuffer())
+
+SUITE["compile"]["time_to_first_SpGeMM"] = @benchmarkable run(cmd)
+
+let
+    A = @fiber d(sl(e(0.0)))
+    B = @fiber d(sl(e(0.0)))
+    C = @fiber d(sl(e(0.0)))
+
+    Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @loop i j k C[i, j] += A[i, k] * B[j, k]))
+
+    SUITE["compile"]["compile_SpGeMM"] = @benchmarkable begin   
+        A, B, C = ($A, $B, $C)
+        Finch.execute_code(:ex, typeof(Finch.@finch_program_instance @loop i j k C[i, j] += A[i, k] * B[j, k]))
+    end
+end
