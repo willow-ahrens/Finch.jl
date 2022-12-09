70cda02b5fce5ff344373e1e3b2707d202e47496
Willow Ahrens <willow.ahrens@mit.edu>
2022-12-09 11:21:31 -0500

    plumbing

--- src/phases.jl
+++ src/phases.jl
@@ -93,7 +93,7 @@ function (ctx::LowerJulia)(root::IndexNode, ::PhaseStyle)
         body = root.body
 
         ext_2 = resolvedim(PhaseStride(ctx, root.idx, root.ext)(body))
-        ext_2 = cache_dim!(ctx, :phase, resolvedim(resultdim(Narrow(root.ext), ext_2)))
+        ext_2 = cache_dim!(ctx, :phase, resolvedim(resultdim(ctx, Narrow(root.ext), ext_2)))
 
         body = PhaseBodyVisitor(ctx, root.idx, root.ext, ext_2)(body)
         body = quote
