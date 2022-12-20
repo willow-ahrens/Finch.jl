f023ac106312a630a01709356287eb2fb9a9a28a
Willow Ahrens <willow@csail.mit.edu>
2022-12-20 01:59:22 -0500

    weird but good

--- src/select.jl
+++ src/select.jl
@@ -83,6 +83,7 @@ function (ctx::LowerJulia)(root, ::SelectStyle)
     idxs = Dict()
     root = SelectVisitor(ctx, idxs)(root)
     for (idx, val) in pairs(idxs)
+        ext = 
         root = @f(
             @loop $idx (
                 @sieve select[$val, $idx] (
@@ -97,7 +98,6 @@ function (ctx::LowerJulia)(root, ::SelectStyle)
     end
 end
 
-#=
 @kwdef struct Furlable
     name = gensym()
     size
@@ -138,12 +138,11 @@ end
 
 function chunkify_access(node, ctx, tns::Furlable)
     if !isempty(node.idxs)
-        idxs = map(ctx, node.idxs)
         if ctx.idx == get_furl_root(node.idxs[1])
-            lpt = tns.body(ctx.ctx, ctx.idx, ctx.ext)
-            lpt = exfurl(lpt, ctx.ctx, node.mode, node.idxs[1])
-            return access(lpt, node.mode, get_furl_root(node.idxs[1]))
+            idxs = map(ctx, node.idxs)
+            return access(tns.body(ctx.ctx, ctx.idx, ctx.ext), node.mode, get_furl_root(node.idxs[1]), idxs[2:end]...)
         else
+            idxs = map(ctx, node.idxs)
             return access(node.tns, node.mode, idxs...)
         end
     end
@@ -160,18 +159,24 @@ function Base.show(io::IO, mime::MIME"text/plain", ex::DiagMask)
 end
 
 virtualize(ex, ::Type{DiagMask}, ctx) = diagmask
+IndexNotation.isliteral(::DiagMask) = false
+Finch.getname(::DiagMask) = gensym()
+Finch.setname(::DiagMask, name) = diagmask
+Finch.getsize(::DiagMask, ctx, mode) = (nodim, nodim)
 
-initialize(::DiagMask, ctx, mode, idxs...) = begin
-    Furlable(
+function initialize!(::DiagMask, ctx, mode, idxs...)
+    tns = Furlable(
+        size = (nodim, nodim),
         body = (ctx, idx, ext) -> Lookup(
-            (i) -> Furlable(
-                body = Pipeline([
+            body = (i) -> Furlable(
+                size = (nodim,),
+                body = (ctx, idx, ext) -> Pipeline([
                     Phase(
-                        stride = (ctx, idx, ext) -> value(:($i - 1)),
+                        stride = (ctx, idx, ext) -> value(:($(ctx(i)) - 1)),
                         body = (start, step) -> Run(body=Simplify(Fill(false)))
                     ),
                     Phase(
-                        stride = (ctx, idx, ext) -> value(i),
+                        stride = (ctx, idx, ext) -> i,
                         body = (start, step) -> Run(body=Simplify(Fill(true))),
                     ),
                     Phase(body = (start, step) -> Run(body=Simplify(Fill(false))))
@@ -179,5 +184,124 @@ initialize(::DiagMask, ctx, mode, idxs...) = begin
             )
         )
     )
+    return access(tns, mode, idxs...)
 end
-=#
+
+struct LoTriMask end
+
+const lotrimask = LoTriMask()
+
+Base.show(io::IO, ex::LoTriMask) = Base.show(io, MIME"text/plain"(), ex)
+function Base.show(io::IO, mime::MIME"text/plain", ex::LoTriMask)
+    print(io, "lotrimask")
+end
+
+virtualize(ex, ::Type{LoTriMask}, ctx) = lotrimask
+IndexNotation.isliteral(::LoTriMask) = false
+Finch.getname(::LoTriMask) = gensym()
+Finch.setname(::LoTriMask, name) = lotrimask
+Finch.getsize(::LoTriMask, ctx, mode) = (nodim, nodim)
+
+function initialize!(::LoTriMask, ctx, mode, idxs...)
+    tns = Furlable(
+        size = (nodim, nodim),
+        body = (ctx, idx, ext) -> Lookup(
+            body = (i) -> Furlable(
+                size = (nodim,),
+                body = (ctx, idx, ext) -> Pipeline([
+                    Phase(
+                        stride = (ctx, idx, ext) -> value(:($(ctx(i)))),
+                        body = (start, step) -> Run(body=Simplify(Fill(true)))
+                    ),
+                    Phase(
+                        body = (start, step) -> Run(body=Simplify(Fill(false))),
+                    )
+                ])
+            )
+        )
+    )
+    return access(tns, mode, idxs...)
+end
+
+struct UpTriMask end
+
+const uptrimask = UpTriMask()
+
+Base.show(io::IO, ex::UpTriMask) = Base.show(io, MIME"text/plain"(), ex)
+function Base.show(io::IO, mime::MIME"text/plain", ex::UpTriMask)
+    print(io, "uptrimask")
+end
+
+virtualize(ex, ::Type{UpTriMask}, ctx) = uptrimask
+IndexNotation.isliteral(::UpTriMask) = false
+Finch.getname(::UpTriMask) = gensym()
+Finch.setname(::UpTriMask, name) = uptrimask
+Finch.getsize(::UpTriMask, ctx, mode) = (nodim, nodim)
+
+function initialize!(::UpTriMask, ctx, mode, idxs...)
+    tns = Furlable(
+        size = (nodim, nodim),
+        body = (ctx, idx, ext) -> Lookup(
+            body = (i) -> Furlable(
+                size = (nodim,),
+                body = (ctx, idx, ext) -> Pipeline([
+                    Phase(
+                        stride = (ctx, idx, ext) -> value(:($(ctx(i)) - 1)),
+                        body = (start, step) -> Run(body=Simplify(Fill(false)))
+                    ),
+                    Phase(
+                        body = (start, step) -> Run(body=Simplify(Fill(true))),
+                    )
+                ])
+            )
+        )
+    )
+    return access(tns, mode, idxs...)
+end
+
+struct BandMask end
+
+const bandmask = BandMask()
+
+Base.show(io::IO, ex::BandMask) = Base.show(io, MIME"text/plain"(), ex)
+function Base.show(io::IO, mime::MIME"text/plain", ex::BandMask)
+    print(io, "bandmask")
+end
+
+bigdim = Extent(literal(-Inf), literal(Inf)) 
+
+virtualize(ex, ::Type{BandMask}, ctx) = bandmask
+IndexNotation.isliteral(::BandMask) = false
+Finch.getname(::BandMask) = gensym()
+Finch.setname(::BandMask, name) = bandmask
+Finch.getsize(::BandMask, ctx, mode) = (nodim, nodim, nodim)
+
+function initialize!(::BandMask, ctx, mode, idxs...)
+    tns = Furlable(
+        size = (nodim, nodim, nodim),
+        body = (ctx, idx, ext) -> Lookup(
+            body = (i) -> Furlable(
+                size = (nodim, nodim),
+                body = (ctx, idx, ext) -> Lookup(
+                    body = (j) -> Furlable(
+                        size = (nodim,),
+                        body = (ctx, idx, ext) -> Pipeline([
+                            Phase(
+                                stride = (ctx, idx, ext) -> value(:($(ctx(i)) - 1)),
+                                body = (start, step) -> Run(body=Simplify(Fill(false)))
+                            ),
+                            Phase(
+                                stride = (ctx, idx, ext) -> j,
+                                body = (start, step) -> Run(body=Simplify(Fill(true)))
+                            ),
+                            Phase(
+                                body = (start, step) -> Run(body=Simplify(Fill(false))),
+                            )
+                        ])
+                    )
+                )
+            )
+        )
+    )
+    return access(tns, mode, idxs...)
+end
