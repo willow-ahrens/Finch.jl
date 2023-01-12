4072bd844580cf515bee1be0cf9a0c3c2438c6d5
Daniel Donenfeld <danieldonenfeld@gmail.com>
2023-01-11 20:33:30 -0500

    Walk and extrude both seem to work (still minimal testing)

--- src/packbitslevels.jl
+++ src/packbitslevels.jl
@@ -98,6 +98,8 @@ mutable struct VirtualPackBitsLevel
     Tv
     I
     pos_alloc
+    pos_fill
+    pos_stop
     headers_alloc
     values_alloc
 end
@@ -106,6 +108,8 @@ function virtualize(ex, ::Type{PackBitsLevel{D, Ti, Th, Tv}}, ctx, tag=:lvl) whe
     I = value(:($sym.I), Int)
     headers_alloc = ctx.freshen(sym, :_headers_alloc)
     pos_alloc = ctx.freshen(sym, :_pos_alloc)
+    pos_fill = ctx.freshen(sym, :_pos_fill)
+    pos_stop = ctx.freshen(sym, :_pos_stop)
     values_alloc = ctx.freshen(sym, :_values_alloc)
     push!(ctx.preamble, quote
         $sym = $ex
@@ -113,7 +117,7 @@ function virtualize(ex, ::Type{PackBitsLevel{D, Ti, Th, Tv}}, ctx, tag=:lvl) whe
         $pos_alloc = length($sym.pos)
         $values_alloc = length($sym.values)
     end)
-    VirtualPackBitsLevel(sym, D, Ti, Th, Tv, I, pos_alloc, headers_alloc, values_alloc)
+    VirtualPackBitsLevel(sym, D, Ti, Th, Tv, I, pos_alloc, pos_fill, pos_stop, headers_alloc, values_alloc)
 end
 function (ctx::Finch.LowerJulia)(lvl::VirtualPackBitsLevel)
     quote
@@ -154,6 +158,8 @@ function initialize_level!(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx::LowerJu
         $(lvl.pos_alloc) = length($(lvl.ex).pos)
         $(lvl.ex).hpos[1] = 1       
         $(lvl.ex).pos[1] = 1
+        $(lvl.pos_fill) = 1
+        $(lvl.pos_stop) = 1
         $(lvl.headers_alloc) = length($(lvl.ex).headers)
         $(lvl.values_alloc) = length($(lvl.ex).values)
     end)
@@ -162,20 +168,67 @@ end
 
 interval_assembly_depth(::VirtualPackBitsLevel) = Inf
 
-#This function is quite simple, since PackBitsLevel don't support reassembly?
-#(Maybe? TODO: check this)
+#This function is quite simple, since PackBitsLevel don't support reassembly
 function assemble!(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode)
     lvl = fbr.lvl
     p_stop = ctx(cache!(ctx, ctx.freshen(lvl.ex, :_p_stop), getstop(envposition(fbr.env))))
     push!(ctx.preamble, quote
         if $(lvl.pos_alloc) < ($p_stop + 1)
             $(lvl.pos_alloc) = $Finch.regrow!($(lvl.ex).pos, $(lvl.pos_alloc), $p_stop + 1)
-            $(lvl.hpos_alloc) = $Finch.regrow!($(lvl.ex).hpos, $(lvl.hpos_alloc), $p_stop + 1)    
+            $Finch.regrow!($(lvl.ex).hpos, $(lvl.pos_alloc), $p_stop + 1)    
         end
+        $(lvl.pos_stop) = $p_stop + 1
     end)
 end
 
 function finalize_level!(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx::LowerJulia, mode)
+    lvl = fbr.lvl
+    my_p = ctx.freshen(:_p)
+    my_pos = ctx.freshen(:_pos_end)
+    my_hpos = ctx.freshen(:_pos_end)
+    rl = ctx.freshen(:_rl)
+
+    high_bit = () -> :($(lvl.Th)(0x1) << $(sizeof(lvl.Th)*8 - 1))
+
+    function record_run_nocheck(run_length, v)
+        quote
+            # println("Recording run of length $($run_length) for value $($v)")
+            $(lvl.headers_alloc) < $my_hpos && ($(lvl.headers_alloc) = $Finch.regrow!($(lvl.ex).headers, $(lvl.headers_alloc), $my_hpos))
+            $(lvl.values_alloc) < $my_pos && ($(lvl.values_alloc) = $Finch.regrow!($(lvl.ex).values, $(lvl.values_alloc), $my_pos))
+            $(lvl.ex).headers[$my_hpos] = $(run_length) & $(high_bit())
+            $(lvl.ex).values[$my_pos] = $v
+            $my_hpos += 1
+            $my_pos += 1
+        end
+    end
+
+    function record_run(ctx, start, stop, v)
+        max_run = :($(high_bit()) - 1)
+        quote
+            $rl = $(ctx(stop)) - $(ctx(start))
+            while $rl >= $(high_bit()) 
+                $(record_run_nocheck(max_run, v))
+                $rl -= $max_run
+            end
+            if $rl != 0 
+                $(record_run_nocheck(rl, v))
+            end
+        end
+    end
+
+
+    push!(ctx.preamble, quote
+        $my_pos = $(lvl.ex).pos[$(lvl.pos_fill)]
+        $my_hpos = $(lvl.ex).hpos[$(lvl.pos_fill)]
+        for $my_p = $(lvl.pos_fill) + 1:$(lvl.pos_stop)
+            $(lvl.values_alloc) < $my_pos && ($(lvl.values_alloc) = $Finch.regrow!($(lvl.ex).values, $(lvl.values_alloc), $my_pos))
+            $(lvl.headers_alloc) < $my_hpos && ($(lvl.headers_alloc) = $Finch.regrow!($(lvl.ex).headers, $(lvl.headers_alloc), $my_hpos))
+            $(record_run(ctx, 1, lvl.I, lvl.D))
+            $(lvl.ex).pos[$(my_p)] = $my_pos
+            $(lvl.ex).hpos[$(my_p)] = $my_hpos
+        end
+    end)
+
     return fbr.lvl
 end
 
@@ -204,38 +257,6 @@ function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Nothing, i
         unfurl(fbr, ctx, mode, extrude, idx, idxs...)
     end
 end
-
-#     int32_t j_crd = 0;
-# int32_t jmtx_mnistrle_0_pos = mtx_covtyperlep_02_pos[i_crd];
-# int32_t pmtx_mnistrle_02_end = mtx_covtyperlep_02_pos[(i_crd + 1)];
-
-
-# while (jmtx_mnistrle_0_pos < pmtx_mnistrle_02_end) {
-#   if (((*((header_type*) &(vals[(jmtx_mnistrle_0_pos)])) >> 15) & 1) == 1) {
-#     header_type mtx_covtyperlep_02_run = *((header_type*) &(vals[(jmtx_mnistrle_0_pos)])) & 0x7FFF;
-#     int32_t mtx_covtyperlep_0_fill_value = ((int32_t*)(&(vals[(jmtx_mnistrle_0_pos + headerSize)])))[0];
-#     jmtx_mnistrle_0_pos += sizeof(int32_t) + headerSize;
-
-#     if (mtx_covtyperlep_0_fill_value){
-#       int32_t acc0 = 0;
-#       const int32_t bound = j_crd + mtx_covtyperlep_02_run;
-#       for (; j_crd < bound; j_crd++) {
-#         acc0 += vec_rand_vals[j_crd];
-#       }
-#       tjout_val += acc0 * mtx_covtyperlep_0_fill_value;
-#     }
-#   }
-#   else {
-#     header_type mtx_covtyperlep_02_found_cnt = *((header_type*) &(vals[(jmtx_mnistrle_0_pos)]));
-#     jmtx_mnistrle_0_pos+= headerSize;
-#     const int32_t bound = j_crd + mtx_covtyperlep_02_found_cnt;
-#     for (;j_crd < bound; j_crd++) {
-#       tjout_val += vec_rand_vals[j_crd] * ((int32_t*)(&(vals[jmtx_mnistrle_0_pos])))[0];
-#       jmtx_mnistrle_0_pos += 4;
-#     }
-#   }
-# }
-
 
 function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Walk, idx, idxs...)
     lvl = fbr.lvl
@@ -274,8 +295,9 @@ function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Walk, idx,
             $my_hend = $(lvl.ex).hpos[$(ctx(envposition(fbr.env))) + 1]
 
             $my_i = 1
-            $my_next_i = 1
+            $my_next_i = 0
             $(incr_coord(:($my_next_i)))
+            # $my_hpos +=1
         end),
         body = Stepper(
             seek = (ctx, ext) -> quote
@@ -296,6 +318,7 @@ function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Walk, idx,
             end,
             body = Thunk(
                 preamble = quote
+                    # println("[321] Next i: $($my_next_i), curr i: $($my_i), pos: $($my_pos), hpos: $($my_hpos)")
                     $my_header = $(lvl.ex).headers[$my_hpos]
                 end,
                 body = Switch([
@@ -304,6 +327,7 @@ function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Walk, idx,
                         stride = (ctx, idx, ext) -> value(my_next_i),
                         chunk = Run(body = Simplify(value(:($(lvl.ex).values[$my_pos]), lvl.Tv))),
                         next = (ctx, idx, ext) -> quote
+                            # println("Run -- Incrementing pos $($my_pos) by 1")
                             $my_pos += 1
                         end
                     ),
@@ -311,11 +335,14 @@ function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Walk, idx,
                         stride = (ctx, idx, ext) -> value(my_next_i),
                         chunk = Lookup(
                             body = (i) -> Thunk(
-                                # preamble = :(println("Looking up at $($my_pos + $(ctx(i))) - 1")),
-                                body = Simplify(value(:($(lvl.ex).values[$my_pos + $(ctx(i)) - 1]), lvl.Tv))
+                                # preamble = quote
+                                #     println("Looking up at $($my_pos + $(ctx(i)) - $my_i -1 ), pos $($my_pos), i $($(ctx(i)))")
+                                # end,
+                                body = Simplify(value(:($(lvl.ex).values[$my_pos + $(ctx(i)) - $my_i - 1]), lvl.Tv))
                             )
                         ),
                         next = (ctx, idx, ext) -> quote
+                            # println("Incrementing pos $($my_pos) by $($(high_bit_mask()) & $my_header)")
                             $my_pos += $(high_bit_mask()) & $my_header
                         end
                     )
@@ -332,21 +359,152 @@ function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Walk, idx,
     exfurl(body, ctx, mode, idx)
 end
 
-# function unfurl(fbr::VirtualFiber{VirtualRepeatRLELevel}, ctx, mode, ::Extrude, idx, idxs...)
-#     lvl = fbr.lvl
-#     tag = lvl.ex
+function unfurl(fbr::VirtualFiber{VirtualPackBitsLevel}, ctx, mode, ::Extrude, idx, idxs...)
+    lvl = fbr.lvl
+    tag = lvl.ex
     
-#     my_pos = ctx.freshen(tag, :_pos)
-#     my_end = ctx.freshen(tag, :_end)
+    my_pos = ctx.freshen(tag, :_pos)
+    my_hpos = ctx.freshen(tag, :_hpos)
+    my_v = ctx.freshen(tag, :_value)
 
-#     my_hpos = ctx.freshen(tag, :_hpos)
-#     my_hend = ctx.freshen(tag, :_hend)
+    my_v_prev = ctx.freshen(tag, :_v_prev)
+    my_i_prev = ctx.freshen(tag, :_i_prev)
+    my_i_start = ctx.freshen(tag, :_i_start)
 
-#     my_i = ctx.freshen(tag, :_i)
-#     my_next_i = ctx.freshen(tag, :_next_i)
-#     my_header = ctx.freshen(tag, :_header_value)
+    my_p = ctx.freshen(tag, :_p)
 
-#     @assert isempty(idxs)
+    rl = ctx.freshen(tag, :_rl)
+    is_dense = ctx.freshen(tag, :_is_dense)
+
+    D = lvl.D
+
+    @assert isempty(idxs)
+
+    high_bit = () -> :($(lvl.Th)(0x1) << $(sizeof(lvl.Th)*8 - 1))
+    high_bit_mask = () -> :(~($(high_bit())))
+
+    function record_run_nocheck(run_length, v)
+        quote
+            # println("Recording unchecked run of length $($run_length) for value $($v) at header position $($my_hpos) and position $($my_pos)")
+            $is_dense = false
+            $(lvl.headers_alloc) < $my_hpos && ($(lvl.headers_alloc) = $Finch.regrow!($(lvl.ex).headers, $(lvl.headers_alloc), $my_hpos))
+            $(lvl.values_alloc) < $my_pos && ($(lvl.values_alloc) = $Finch.regrow!($(lvl.ex).values, $(lvl.values_alloc), $my_pos))
+            $(lvl.ex).headers[$my_hpos] = $(run_length) | $(high_bit())
+            $(lvl.ex).values[$my_pos] = $v
+            $my_hpos += 1
+            $my_pos += 1
+        end
+    end
+
+    function append_val(v)
+        quote
+            if $is_dense 
+                $(lvl.ex).headers[$my_hpos - 1] += 1 
+            else 
+                $is_dense = true
+                $(lvl.headers_alloc) < $my_hpos && ($(lvl.headers_alloc) = $Finch.regrow!($(lvl.ex).headers, $(lvl.headers_alloc), $my_hpos))
+                $(lvl.ex).headers[$my_hpos] = 1
+                $my_hpos += 1
+            end
+            $(lvl.values_alloc) < $my_pos && ($(lvl.values_alloc) = $Finch.regrow!($(lvl.ex).values, $(lvl.values_alloc), $my_pos))
+            $(lvl.ex).values[$my_pos] = $v
+            $my_pos += 1
+
+        end
+    end
+
+    function record_run(ctx, start, stop, v)
+        # println("ctx: $ctx")
+        max_run = :($(high_bit()) - 1)
+        quote
+            $rl = $(ctx(stop)) - $(ctx(start)) + 1
+            if $rl == 1
+                $(append_val(v))
+            else
+                # println("Recording checked run from $($(ctx(start))) to $($(ctx(stop))) ($($rl)) for value $($v) at header position $($my_hpos) and position $($my_pos)")
+                while $rl >= $(high_bit()) 
+                    $(record_run_nocheck(max_run, v))
+                    $rl -= $max_run
+                end
+                if $rl != 0 
+                    $(record_run_nocheck(rl, v))
+                end
+            end
+        end
+    end
+
+    push!(ctx.preamble, quote
+        $my_pos = $(lvl.ex).pos[$(lvl.pos_fill)]
+        $my_hpos = $(lvl.ex).hpos[$(lvl.pos_fill)]
+        for $my_p = $(lvl.pos_fill) + 1:$(ctx(envposition(fbr.env)))
+            $(record_run(ctx, literal(1), lvl.I, D))
+            $(lvl.ex).pos[$(my_p)] = $my_pos
+            $(lvl.ex).hpos[$(my_p)] = $my_hpos
+        end
+        $my_i_start = 1
+        $my_i_prev = 0
+        $my_v_prev = $D
+        $is_dense = false
+    end)
 
 
-# end
+    body = AcceptRun(
+        val = D,
+        body = (ctx, start, stop) -> Thunk(
+            preamble = quote
+                # println("[preamble] my_v_prev: $($my_v_prev), my_i_prev: $($my_i_prev), my_i_start: $($my_i_start), start: $($(ctx(start))), stop: $($(ctx(stop))), pos: $($my_pos), hpos: $($my_pos)")
+                if $my_v_prev != $D && ($my_i_prev + 1) < $(ctx(start))
+                    # println("Preamble 429")
+                    $(record_run(ctx, my_i_start, my_i_prev, my_v_prev))
+                    $my_v_prev = $D
+                    $my_i_start = $(ctx(start)) - 1
+                end
+                $my_i_prev = $(ctx(start)) - 1
+                $my_v = $D
+            end,
+            body = Thunk( 
+                # preamble = :(println("[body] my_v_prev: $($my_v_prev), my_i_prev: $($my_i_prev), my_i_start: $($my_i_start), start: $($(ctx(start))), stop: $($(ctx(stop))), pos: $($my_pos), hpos: $($my_pos)")),
+                body = Simplify(Fill(value(my_v, lvl.Tv), D))
+            ),
+            epilogue = begin
+                body = quote
+                    # println("[epilogue] my_v_prev: $($my_v_prev), my_i_prev: $($my_i_prev), my_i_start: $($my_i_start), start: $($(ctx(start))), stop: $($(ctx(stop))), pos: $($my_pos), hpos: $($my_pos)")
+                    if $my_v_prev != $my_v && $my_i_prev > 0
+                        # println("epilogue 445")
+                        $(record_run(ctx, my_i_start, my_i_prev, my_v_prev))
+                        $my_i_start = $(ctx(stop))
+                    end
+                    $my_v_prev = $my_v
+                    $my_i_prev = $(ctx(stop))
+                end
+                if envdefaultcheck(fbr.env) !== nothing
+                    body = quote
+                        $body
+                        $(envdefaultcheck(fbr.env)) = false
+                    end
+                end
+                body
+            end
+        )
+    )
+
+    push!(ctx.epilogue, quote
+        # println("my_v_prev: $($my_v_prev), my_i_prev: $($my_i_prev), my_i_start: $($my_i_start), pos: $($my_pos), hpos: $($my_pos)")
+
+        if $my_v_prev != $D && $my_i_prev < $(ctx(lvl.I))
+            # println("ctx epilogue 467")
+            $(record_run(ctx, my_i_start, my_i_prev, my_v_prev))
+            if $my_i_prev != $(ctx(lvl.I))
+                $(record_run(ctx, my_i_prev, lvl.I, D))
+            end
+        elseif $(ctx(lvl.I)) > 0
+            # println("ctx epilogue 478: my_i_prev $($my_i_prev), my_i_start $($my_i_start), my_hpos $($my_hpos),  header $($(lvl.ex).headers[$my_hpos - 1])")
+            $(record_run(ctx, my_i_start, lvl.I, my_v_prev))
+        end
+        $(lvl.ex).pos[$(ctx(envposition(fbr.env))) + 1] = $my_pos
+        $(lvl.ex).hpos[$(ctx(envposition(fbr.env))) + 1] = $my_hpos
+        $(lvl.pos_fill) = $(ctx(envposition(fbr.env))) + 1
+    end)
+
+    exfurl(body, ctx, mode, idx)
+end
