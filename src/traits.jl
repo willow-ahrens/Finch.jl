0d0fa4c92c27dbcb0b4bb2e64629e87b24641b59
Willow Ahrens <willow@csail.mit.edu>
2023-02-06 11:33:02 -0500

    mostly good, normalizes

--- src/traits.jl
+++ src/traits.jl
@@ -10,17 +10,25 @@ struct MaybeFill
     lvl
 end
 
-struct ElementData end
-struct RepeatData end
+struct ElementData
+    eltype
+    default
+end
 
-function getindex_output(lvl::SparseData, ind, inds...)
+struct RepeatData
+    eltype
+    default
+end
+
+getindex_output(lvl::SparseData, ind, inds...) =
     sublvl = getindex_output(lvl.lvl, inds...)
     ndims(ind) == 0 && return MaybeFill(sublvl)
     return getindex_output_sparse(sublvl, ind)
 end
-getindex_output_sparse(sublvl::MaybeFill, ind) = getindex_output_sparse(sublvl.lvl, ind)
+getindex_output_sparse(sublvl::MaybeFill, ind) = MaybeFill(SparseData(sublvl.lvl))
 getindex_output_sparse(sublvl, ind) = MaybeFill(SparseData(sublvl))
 getindex_output_sparse(sublvl, ind::Base.Slice) = SparseData(sublvl)
+getindex_output_sparse(sublvl::MaybeFill, ind::Base.Slice) = MaybeFill(SparseData(sublvl.lvl))
 
 function getindex_output(lvl::DenseData, ind, inds...)
     sublvl = getindex_output(lvl.lvl, inds...)
@@ -28,14 +36,14 @@ function getindex_output(lvl::DenseData, ind, inds...)
     return getindex_output_dense(sublvl, ind)
 end
 getindex_output_dense(sublvl, ind) = DenseData(sublvl)
-getindex_output_dense(sublvl::MaybeFill, ind) = SparseData(sublvl.lvl)
+getindex_output_dense(sublvl::MaybeFill, ind) = MaybeFill(SparseData(sublvl.lvl))
 
 getindex_output(lvl::ElementData) = lvl
 
-function getindex_output(lvl::RepeatData, ind, inds...)
-    sublvl = ElementData()
+function getindex_output(lvl::RepeatData, ind)
+    sublvl = ElementData(lvl.eltype, lvl.default)
     ndims(ind) == 0 && return sublvl
     return getindex_output_repeat(sublvl, ind)
 end
 getindex_output_repeat(sublvl, ind) = DenseData(sublvl)
-getindex_output_repeat(sublvl, ind::Base.AbstractUnitRange) = RepeatData()
+getindex_output_repeat(sublvl, ind::Base.AbstractUnitRange) = RepeatData(sublvl.eltype, sublvl.default)
