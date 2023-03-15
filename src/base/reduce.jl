e9c522725c15c5533af0142ba0af04742a5d7673
Willow Ahrens <willow@csail.mit.edu>
2023-03-15 16:11:57 -0400

    add reduction

--- src/base/reduce.jl
+++ src/base/reduce.jl
@@ -8,23 +8,45 @@ by `tns` on `dims` by `op`.
 """
 function reduce_rep(op, rep, dims) end
 
-reduce_rep(op, tns, dims) = reduce_rep_def(op, tns, map(n -> n in dims ? Drop(n) : n, 1:ndims(tns))...)
+reduce_rep(op, tns, dims) =
+    reduce_rep_def(op, tns, map(n -> n in dims ? Drop(n) : n, 1:ndims(tns))...)
 
-reduce_rep_def(op, fbr::HollowData, idxs...) = HollowData(reduce_rep_def(op, fbr.lvl, idxs...))
+function fixpoint_type(op, z, tns)
+    S = Union{}
+    T = typeof(z)
+    while T != S
+        S = T
+        T = Union{T, Base.combine_eltypes(op, (T, eltype(tns)))}
+    end
+    T
+end
 
-reduce_rep_def(op, lvl::SparseData, idx::Drop, idxs...) = HollowData(reduce_rep_def(op, lvl.lvl, idxs...))
-reduce_rep_def(op, lvl::SparseData, idx, idxs...) = HollowData(SparseData(reduce_rep_def(op, lvl.lvl, idxs...)))
-reduce_rep_def(op, lvl::SparseData, idx::Type{<:Base.Slice}, idxs...) = SparseData(reduce_rep_def(op, lvl.lvl, idxs...))
+reduce_rep_def(op, z, fbr::HollowData, idxs...) = HollowData(reduce_rep_def(op, z, fbr.lvl, idxs...))
+function reduce_rep_def(op, z, lvl::HollowData, idx, idxs...)
+    if op(z, default(lvl)) == z
+        HollowData(reduce_rep_def(op, z, lvl.lvl, idxs...))
+    else
+        HollowData(reduce_rep_def(op, z, lvl.lvl, idxs...))
+    end
+end
 
-reduce_rep_def(op, lvl::DenseData, idx::Drop, idxs...) = reduce_rep_def(op, lvl.lvl, idxs...)
-reduce_rep_def(op, lvl::DenseData, idx, idxs...) = SolidData(reduce_rep_def(op, lvl.lvl, idxs...))
+reduce_rep_def(op, z, lvl::SparseData, idx::Drop, idxs...) = reduce_rep_def(op, z, lvl.lvl, idxs...)
+function reduce_rep_def(op, z, lvl::SparseData, idx, idxs...)
+    if op(z, default(lvl)) == z
+        SparseData(reduce_rep_def(op, z, lvl.lvl, idxs...))
+    else
+        DenseData(reduce_rep_def(op, z, lvl.lvl, idxs...))
+    end
+end
 
-reduce_rep_def(op, lvl::ElementData) = lvl
+reduce_rep_def(op, z, lvl::DenseData, idx::Drop, idxs...) = reduce_rep_def(op, z, lvl.lvl, idxs...)
+reduce_rep_def(op, z, lvl::DenseData, idx, idxs...) = DenseData(reduce_rep_def(op, z, lvl.lvl, idxs...))
 
-reduce_rep_def(op, lvl::RepeatData, idx::Drop) = SolidData(ElementData(lvl.default, lvl.eltype))
-reduce_rep_def(op, lvl::RepeatData, idx) = SolidData(ElementData(lvl.default, lvl.eltype))
-reduce_rep_def(op, lvl::RepeatData, idx::Type{<:AbstractUnitRange}) = SolidData(ElementData(lvl.default, lvl.eltype))
+reduce_rep_def(op, z, lvl::ElementData) = ElementData(op, z, fixpoint_type(op, z, lvl))
+
+reduce_rep_def(op, z, lvl::RepeatData, idx::Drop) = reduce_rep_def(op, ElementData(lvl.default, lvl.eltype))
+reduce_rep_def(op, z, lvl::RepeatData, idx) = RepeatData(op, z, fixpoint_type(op, z))
 
 function reduce!(op, bc::Broadcasted{FinchStyle{N}}, dims) where {N}
-    T = Base.combine_eltypes(bc.f, bc.args::Tuple)
+
 end
