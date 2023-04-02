8ec9db61e5a6c3c4bf6cdd1309a4dc8ff1b88c22
Cecilia Chen <cece2013c@gmail.com>
2023-04-02 17:19:06 -0400

    forgot to commit benchmarks file

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,6 +9,7 @@ using Finch
 using BenchmarkTools
 using MatrixDepot
 using SparseArrays
+include("../Apps/apps.jl")
 
 SUITE = BenchmarkGroup()
 
@@ -61,107 +62,20 @@ end
 
 SUITE["graphs"] = BenchmarkGroup()
 
-function pagerank(edges; nsteps=20, damp = 0.85)
-    (n, m) = size(edges)
-    @assert n == m
-    out_degree = @fiber d(e(0))
-    @finch (out_degree .= 0; @loop j i out_degree[j] += edges[i, j])
-    scaled_edges = @fiber d(sl(e(0.0)))
-    @finch (scaled_edges .= 0; @loop j i scaled_edges[i, j] = ifelse(out_degree[i] != 0, edges[i, j] / out_degree[j], 0))
-    r = @fiber d(e(0.0), n)
-    @finch (r .= 0; @loop j r[j] = 1.0/n)
-    rank = @fiber d(e(0.0), n)
-    beta_score = (1 - damp)/n
-
-    for step = 1:nsteps
-        @finch (rank .= 0; @loop j i rank[i] += scaled_edges[i, j] * r[j])
-        @finch (r .= 0; @loop i r[i] = beta_score + damp * rank[i])
-    end
-    return r
-end
-
 SUITE["graphs"]["pagerank"] = BenchmarkGroup()
 for mtx in ["SNAP/soc-Epinions1", "SNAP/soc-LiveJournal1"]
-    SUITE["graphs"]["pagerank"][mtx] = @benchmarkable pagerank($(fiber(SparseMatrixCSC(matrixdepot(mtx))))) 
-end
-
-function bfs(edges, source=5)
-    (n, m) = size(edges)
-    edges = pattern!(edges)
-
-    @assert n == m
-    F = @fiber sbm(p(), n)
-    _F = @fiber sbm(p(), n)
-    @finch F[source] = true
-
-    V = @fiber d(e(false), n)
-    @finch V[source] = true
-
-    P = @fiber d(e(0), n)
-    @finch P[source] = source
-
-    v = Scalar(false)
-
-    while countstored(F) > 0
-        @finch begin
-            _F .= false
-            @loop j k begin
-                v .= false
-                v[] = F[j] && edges[k, j] && !(V[k])
-                if v[]
-                    _F[k] |= true
-                    P[k] <<choose(0)>>= j #Only set the parent for this vertex
-                end
-            end
-        end
-        @finch @loop k V[k] |= _F[k]
-        (F, _F) = (_F, F)
-    end
-    return F
+    SUITE["graphs"]["pagerank"][mtx] = @benchmarkable Apps.pagerank($(fiber(SparseMatrixCSC(matrixdepot(mtx))))) 
 end
 
 SUITE["graphs"]["bfs"] = BenchmarkGroup()
 for mtx in ["SNAP/soc-Epinions1", "SNAP/soc-LiveJournal1"]
-    SUITE["graphs"]["bfs"][mtx] = @benchmarkable bfs($(fiber(SparseMatrixCSC(matrixdepot(mtx))))) 
+    SUITE["graphs"]["bfs"][mtx] = @benchmarkable Apps.bfs($(fiber(SparseMatrixCSC(matrixdepot(mtx))))) 
 end
-
-# Inputs:
-#   edges: 2D matrix of edge weights with Inf for unconnected edges
-#   source: vertex to start
-# Output:
-#   dists: an array of tuples (d, n) where d is the shortest distance and n the parent node in the path
-#          or -1 if a negative weight cycle is found
-function bellmanford(edges, source=1)
-    (n, m) = size(edges)
-    @assert n == m
-
-    init_dists = [(Inf, -1) for i=1:n]
-    init_dists[source] = (0.0, -1)
-    dists_prev = @fiber(d(e((Inf, -1))), init_dists)
-    dists_buffer = @fiber(d(e((Inf, -1)), n))
-    dists_next = @fiber(d(e((Inf, -1)), n))
-    modified = Scalar(false)
-
-    for iter = 1:n  
-        @finch @loop j i dists_buffer[j] <<min>>= (first(dists_prev[i]) + edges[i, j], i)
-        @finch @loop j dists_next[j] = min(dists_prev[j], dists_buffer[j])
-
-        modified = Scalar(false)
-        @finch @loop i modified[] |= dists_next[i][1] != dists_prev[i][1]
-        if !modified[]
-            break
-        end
-        dists_prev, dists_next = dists_next, dists_prev
-    end
-
-    return modified[] ? -1 : dists_prev
-end
-
 
 SUITE["graphs"]["bellmanford"] = BenchmarkGroup()
-for mtx in ["Newman/netscience"] #, "Williams/pdb1HYS"]#, "GAP/GAP-road"]
-    A = redefault!(fiber(SparseMatrixCSC(matrixdepot(mtx))), 0.0)
-    SUITE["graphs"]["bellmanford"][mtx] = @benchmarkable bellmanford($A)
+for mtx in ["Newman/netscience", "Williams/pdb1HYS"]#, "GAP/GAP-road"]
+    A = redefault!(fiber(SparseMatrixCSC(matrixdepot(mtx))), Inf)
+    SUITE["graphs"]["bellmanford"][mtx] = @benchmarkable Apps.bellmanford($A)
 end
 
 SUITE["matrices"] = BenchmarkGroup()
