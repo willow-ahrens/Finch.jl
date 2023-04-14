0894a947200edd1dcaefd18e0c6a6429612ceec4
Documenter.jl <documenter@juliadocs.github.io>
2023-04-14 21:40:16 +0000

    build based on 5cd0567

---
+++ dev/interactive.jl
@@ -0,0 +1,20 @@
+# 
+# It's easy to generate sparse code with Finch, give it a try!
+# 
+# A note to visitors using Binder: it may take a minute or
+# two to compile the first kernel, perhaps enjoy a nice little coffee break ^.^
+#
+
+using Finch
+
+A = fsprand((10, 10), 0.5)
+
+x = rand(10)
+y = rand(10)
+
+@finch_code begin
+    y .= 0
+    for i = _, j = _
+        y[i] += A[i, j] * x[j]
+    end
+end
