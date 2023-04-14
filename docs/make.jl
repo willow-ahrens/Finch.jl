bdf688687b069698c2cc4f645239c6dbcb779dd1
Willow Ahrens <willow.ahrens@mit.edu>
2023-04-14 17:15:22 -0400

    go back to the old way

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Literate
 
 DocMeta.setdocmeta!(Finch, :DocTestSetup, :(using Finch; using SparseArrays); recursive=true)
 
-Literate.notebook(joinpath(@__DIR__, "src/usage.jl"), joinpath(@__DIR__, "../binder"))
+Literate.notebook(joinpath(@__DIR__, "src/usage.jl"), joinpath(@__DIR__, "src/generated"))
 
 makedocs(;
     modules=[Finch],
