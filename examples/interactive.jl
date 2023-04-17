f3336244787f6426b16ebfe5c6da94e99324fa5d
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-04-17 00:36:43 -0400

    hmm

--- examples/interactive.jl
+++ examples/interactive.jl
@@ -1,5 +1,5 @@
 # 
-# It's easy to generate sparse code with Finch, give it a try!
+# An example generating sparse code with Finch, make some changes and give it a try!
 # 
 # A note to visitors using Binder: it may take a minute or
 # two to compile the first kernel, perhaps enjoy a nice little coffee break ^.^
@@ -7,14 +7,14 @@
 
 using Finch
 
-## Construct a CSR matrix of uniformly random nonzeros
-A = @fiber(d(sl(e(0.0))), fsprand((10, 10), 0.5))
+## Construct a CSR sparse input matrix (20% random nonzeros)
+A = @fiber(d(sl(e(0.0))), fsprand((10, 10), 0.2))
 
-## Construct a dense vector input and output
+## Construct a dense vector input and output (all random values)
 x = rand(10)
 y = rand(10)
 
-## Matrix-vector multiply y = A * x
+## Emit code for matrix-vector multiply y = A * x
 @finch_code begin
     for i = _, j = _
         y[i] += A[i, j] * x[j]
