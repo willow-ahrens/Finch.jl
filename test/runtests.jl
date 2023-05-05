6d7b856d9462407910242591d169b9364a9023fc
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-05-05 12:25:54 -0400

    oops.

--- test/runtests.jl
+++ test/runtests.jl
@@ -83,7 +83,6 @@ include("utils.jl")
     if should_run("issues") include("test_issues.jl") end
     if should_run("base") include("test_base.jl") end
     if should_run("apps") include("test_apps.jl") end
-    if should_run("meta") include("test_meta.jl") end
     if should_run("fileio") include("test_fileio.jl") end
     if should_run("embed") include("embed/test_embed.jl") end
     #algebra goes at the end since it calls refresh()
