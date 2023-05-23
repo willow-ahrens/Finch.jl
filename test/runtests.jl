ee684b536ca48a24f62f91167043ceb6cb6ef51e
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-05-23 15:21:15 -0400

    add test deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,11 @@
+#!/usr/bin/env julia
+if abspath(PROGRAM_FILE) == @__FILE__
+    using Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.resolve()
+    Pkg.instantiate()
+end
+
 using Test
 using ArgParse
 
