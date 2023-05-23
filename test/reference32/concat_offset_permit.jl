7a54c9dce0b8536ba0a83c6af40971abc239a79a
Willow Ahrens <willow@lanka.csail.mit.edu>
2023-05-23 12:01:31 -0400

    add 32-bit reference output

--- test/reference32/concat_offset_permit.jl
+++ test/reference32/concat_offset_permit.jl
@@ -27,42 +27,42 @@ begin
         end
         i = phase_stop + 1
     end
-    phase_stop_2 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl.shape, i_stop, 0)
+    phase_stop_2 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta, A_lvl.shape, i_stop)
     if phase_stop_2 >= i
-        B_lvl_q = B_lvl.ptr[1]
-        B_lvl_q_stop = B_lvl.ptr[1 + 1]
-        if B_lvl_q < B_lvl_q_stop
-            B_lvl_i1 = B_lvl.idx[B_lvl_q_stop - 1]
+        A_lvl_q = A_lvl.ptr[1]
+        A_lvl_q_stop = A_lvl.ptr[1 + 1]
+        if A_lvl_q < A_lvl_q_stop
+            A_lvl_i1 = A_lvl.idx[A_lvl_q_stop - 1]
         else
-            B_lvl_i1 = 0
+            A_lvl_i1 = 0
         end
-        phase_stop_3 = min(phase_stop_2, (((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1)
+        phase_stop_3 = min(A_lvl_i1, phase_stop_2)
         if phase_stop_3 >= i
-            if B_lvl.idx[B_lvl_q] < i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
-                B_lvl_q = scansearch(B_lvl.idx, i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta), B_lvl_q, B_lvl_q_stop - 1)
+            if A_lvl.idx[A_lvl_q] < i
+                A_lvl_q = scansearch(A_lvl.idx, i, A_lvl_q, A_lvl_q_stop - 1)
             end
             while i <= phase_stop_3
-                B_lvl_i = B_lvl.idx[B_lvl_q]
-                phase_stop_4 = min(phase_stop_3, (((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i)
-                if B_lvl_i == phase_stop_4 + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
-                    B_lvl_2_val_2 = B_lvl_2.val[B_lvl_q]
+                A_lvl_i = A_lvl.idx[A_lvl_q]
+                phase_stop_4 = min(phase_stop_3, A_lvl_i)
+                if A_lvl_i == phase_stop_4
+                    A_lvl_2_val_2 = A_lvl_2.val[A_lvl_q]
                     if C_lvl_qos > C_lvl_qos_stop
                         C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
                         resize_if_smaller!(C_lvl.idx, C_lvl_qos_stop)
                         resize_if_smaller!(C_lvl_2.val, C_lvl_qos_stop)
                         fill_range!(C_lvl_2.val, 0.0, C_lvl_qos, C_lvl_qos_stop)
                     end
-                    C_lvl_2.val[C_lvl_qos] = B_lvl_2_val_2
+                    C_lvl_2.val[C_lvl_qos] = A_lvl_2_val_2
                     C_lvl.idx[C_lvl_qos] = phase_stop_4
                     C_lvl_qos += 1
-                    B_lvl_q += 1
+                    A_lvl_q += 1
                 end
                 i = phase_stop_4 + 1
             end
         end
         i = phase_stop_2 + 1
     end
-    phase_stop_6 = min(i_stop, 0)
+    phase_stop_6 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta, i_stop)
     if phase_stop_6 >= i
         for i_13 = i:phase_stop_6
             if C_lvl_qos > C_lvl_qos_stop
@@ -77,35 +77,35 @@ begin
         end
         i = phase_stop_6 + 1
     end
-    phase_stop_7 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta, A_lvl.shape, i_stop)
+    phase_stop_7 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl.shape, i_stop, 0)
     if phase_stop_7 >= i
-        A_lvl_q = A_lvl.ptr[1]
-        A_lvl_q_stop = A_lvl.ptr[1 + 1]
-        if A_lvl_q < A_lvl_q_stop
-            A_lvl_i1 = A_lvl.idx[A_lvl_q_stop - 1]
+        B_lvl_q = B_lvl.ptr[1]
+        B_lvl_q_stop = B_lvl.ptr[1 + 1]
+        if B_lvl_q < B_lvl_q_stop
+            B_lvl_i1 = B_lvl.idx[B_lvl_q_stop - 1]
         else
-            A_lvl_i1 = 0
+            B_lvl_i1 = 0
         end
-        phase_stop_8 = min(A_lvl_i1, phase_stop_7)
+        phase_stop_8 = min(phase_stop_7, (((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1)
         if phase_stop_8 >= i
-            if A_lvl.idx[A_lvl_q] < i
-                A_lvl_q = scansearch(A_lvl.idx, i, A_lvl_q, A_lvl_q_stop - 1)
+            if B_lvl.idx[B_lvl_q] < i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
+                B_lvl_q = scansearch(B_lvl.idx, i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta), B_lvl_q, B_lvl_q_stop - 1)
             end
             while i <= phase_stop_8
-                A_lvl_i = A_lvl.idx[A_lvl_q]
-                phase_stop_9 = min(phase_stop_8, A_lvl_i)
-                if A_lvl_i == phase_stop_9
-                    A_lvl_2_val_2 = A_lvl_2.val[A_lvl_q]
+                B_lvl_i = B_lvl.idx[B_lvl_q]
+                phase_stop_9 = min(phase_stop_8, (((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i)
+                if B_lvl_i == phase_stop_9 + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
+                    B_lvl_2_val_2 = B_lvl_2.val[B_lvl_q]
                     if C_lvl_qos > C_lvl_qos_stop
                         C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
                         resize_if_smaller!(C_lvl.idx, C_lvl_qos_stop)
                         resize_if_smaller!(C_lvl_2.val, C_lvl_qos_stop)
                         fill_range!(C_lvl_2.val, 0.0, C_lvl_qos, C_lvl_qos_stop)
                     end
-                    C_lvl_2.val[C_lvl_qos] = A_lvl_2_val_2
+                    C_lvl_2.val[C_lvl_qos] = B_lvl_2_val_2
                     C_lvl.idx[C_lvl_qos] = phase_stop_9
                     C_lvl_qos += 1
-                    A_lvl_q += 1
+                    B_lvl_q += 1
                 end
                 i = phase_stop_9 + 1
             end
@@ -128,7 +128,7 @@ begin
         else
             B_lvl_i1 = 0
         end
-        phase_stop_12 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1, A_lvl_i1, phase_stop_11)
+        phase_stop_12 = min(A_lvl_i1, (((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1, phase_stop_11)
         if phase_stop_12 >= i
             if A_lvl.idx[A_lvl_q] < i
                 A_lvl_q = scansearch(A_lvl.idx, i, A_lvl_q, A_lvl_q_stop - 1)
@@ -139,7 +139,7 @@ begin
             while i <= phase_stop_12
                 A_lvl_i = A_lvl.idx[A_lvl_q]
                 B_lvl_i = B_lvl.idx[B_lvl_q]
-                phase_stop_13 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i, A_lvl_i, phase_stop_12)
+                phase_stop_13 = min(A_lvl_i, (((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i, phase_stop_12)
                 if A_lvl_i == phase_stop_13 && B_lvl_i == phase_stop_13 + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
                     A_lvl_2_val_3 = A_lvl_2.val[A_lvl_q]
                     B_lvl_2_val_3 = B_lvl_2.val[B_lvl_q]
@@ -173,15 +173,19 @@ begin
             end
             i = phase_stop_12 + 1
         end
-        phase_stop_14 = min(A_lvl_i1, phase_stop_11)
+        phase_stop_14 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1, phase_stop_11)
         if phase_stop_14 >= i
+            i = phase_stop_14 + 1
+        end
+        phase_stop_15 = min(A_lvl_i1, phase_stop_11)
+        if phase_stop_15 >= i
             if A_lvl.idx[A_lvl_q] < i
                 A_lvl_q = scansearch(A_lvl.idx, i, A_lvl_q, A_lvl_q_stop - 1)
             end
-            while i <= phase_stop_14
+            while i <= phase_stop_15
                 A_lvl_i = A_lvl.idx[A_lvl_q]
-                phase_stop_15 = min(A_lvl_i, phase_stop_14)
-                if A_lvl_i == phase_stop_15
+                phase_stop_16 = min(A_lvl_i, phase_stop_15)
+                if A_lvl_i == phase_stop_16
                     A_lvl_2_val_4 = A_lvl_2.val[A_lvl_q]
                     if C_lvl_qos > C_lvl_qos_stop
                         C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
@@ -190,51 +194,51 @@ begin
                         fill_range!(C_lvl_2.val, 0.0, C_lvl_qos, C_lvl_qos_stop)
                     end
                     C_lvl_2.val[C_lvl_qos] = coalesce(A_lvl_2_val_4, 0.0)
-                    C_lvl.idx[C_lvl_qos] = phase_stop_15
+                    C_lvl.idx[C_lvl_qos] = phase_stop_16
                     C_lvl_qos += 1
                     A_lvl_q += 1
                 end
-                i = phase_stop_15 + 1
+                i = phase_stop_16 + 1
             end
         end
         i = phase_stop_11 + 1
     end
-    phase_stop_18 = min(A_lvl.shape, i_stop)
+    phase_stop_18 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl.shape, i_stop)
     if phase_stop_18 >= i
-        A_lvl_q = A_lvl.ptr[1]
-        A_lvl_q_stop = A_lvl.ptr[1 + 1]
-        if A_lvl_q < A_lvl_q_stop
-            A_lvl_i1 = A_lvl.idx[A_lvl_q_stop - 1]
+        B_lvl_q = B_lvl.ptr[1]
+        B_lvl_q_stop = B_lvl.ptr[1 + 1]
+        if B_lvl_q < B_lvl_q_stop
+            B_lvl_i1 = B_lvl.idx[B_lvl_q_stop - 1]
         else
-            A_lvl_i1 = 0
+            B_lvl_i1 = 0
         end
-        phase_stop_19 = min(A_lvl_i1, phase_stop_18)
+        phase_stop_19 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1, phase_stop_18)
         if phase_stop_19 >= i
-            if A_lvl.idx[A_lvl_q] < i
-                A_lvl_q = scansearch(A_lvl.idx, i, A_lvl_q, A_lvl_q_stop - 1)
+            if B_lvl.idx[B_lvl_q] < i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
+                B_lvl_q = scansearch(B_lvl.idx, i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta), B_lvl_q, B_lvl_q_stop - 1)
             end
             while i <= phase_stop_19
-                A_lvl_i = A_lvl.idx[A_lvl_q]
-                phase_stop_20 = min(A_lvl_i, phase_stop_19)
-                if A_lvl_i == phase_stop_20
-                    A_lvl_2_val_5 = A_lvl_2.val[A_lvl_q]
+                B_lvl_i = B_lvl.idx[B_lvl_q]
+                phase_stop_20 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i, phase_stop_19)
+                if B_lvl_i == phase_stop_20 + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
+                    B_lvl_2_val_4 = B_lvl_2.val[B_lvl_q]
                     if C_lvl_qos > C_lvl_qos_stop
                         C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
                         resize_if_smaller!(C_lvl.idx, C_lvl_qos_stop)
                         resize_if_smaller!(C_lvl_2.val, C_lvl_qos_stop)
                         fill_range!(C_lvl_2.val, 0.0, C_lvl_qos, C_lvl_qos_stop)
                     end
-                    C_lvl_2.val[C_lvl_qos] = A_lvl_2_val_5
+                    C_lvl_2.val[C_lvl_qos] = B_lvl_2_val_4
                     C_lvl.idx[C_lvl_qos] = phase_stop_20
                     C_lvl_qos += 1
-                    A_lvl_q += 1
+                    B_lvl_q += 1
                 end
                 i = phase_stop_20 + 1
             end
         end
         i = phase_stop_18 + 1
     end
-    phase_stop_22 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta, i_stop)
+    phase_stop_22 = min(i_stop, 0)
     if phase_stop_22 >= i
         for i_35 = i:phase_stop_22
             if C_lvl_qos > C_lvl_qos_stop
@@ -249,35 +253,35 @@ begin
         end
         i = phase_stop_22 + 1
     end
-    phase_stop_23 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl.shape, i_stop)
+    phase_stop_23 = min(A_lvl.shape, i_stop)
     if phase_stop_23 >= i
-        B_lvl_q = B_lvl.ptr[1]
-        B_lvl_q_stop = B_lvl.ptr[1 + 1]
-        if B_lvl_q < B_lvl_q_stop
-            B_lvl_i1 = B_lvl.idx[B_lvl_q_stop - 1]
+        A_lvl_q = A_lvl.ptr[1]
+        A_lvl_q_stop = A_lvl.ptr[1 + 1]
+        if A_lvl_q < A_lvl_q_stop
+            A_lvl_i1 = A_lvl.idx[A_lvl_q_stop - 1]
         else
-            B_lvl_i1 = 0
+            A_lvl_i1 = 0
         end
-        phase_stop_24 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i1, phase_stop_23)
+        phase_stop_24 = min(A_lvl_i1, phase_stop_23)
         if phase_stop_24 >= i
-            if B_lvl.idx[B_lvl_q] < i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
-                B_lvl_q = scansearch(B_lvl.idx, i + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta), B_lvl_q, B_lvl_q_stop - 1)
+            if A_lvl.idx[A_lvl_q] < i
+                A_lvl_q = scansearch(A_lvl.idx, i, A_lvl_q, A_lvl_q_stop - 1)
             end
             while i <= phase_stop_24
-                B_lvl_i = B_lvl.idx[B_lvl_q]
-                phase_stop_25 = min((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta + B_lvl_i, phase_stop_24)
-                if B_lvl_i == phase_stop_25 + -((((ex.bodies[2]).body.rhs.args[2]).idxs[1]).tns.tns.delta)
-                    B_lvl_2_val_4 = B_lvl_2.val[B_lvl_q]
+                A_lvl_i = A_lvl.idx[A_lvl_q]
+                phase_stop_25 = min(A_lvl_i, phase_stop_24)
+                if A_lvl_i == phase_stop_25
+                    A_lvl_2_val_5 = A_lvl_2.val[A_lvl_q]
                     if C_lvl_qos > C_lvl_qos_stop
                         C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
                         resize_if_smaller!(C_lvl.idx, C_lvl_qos_stop)
                         resize_if_smaller!(C_lvl_2.val, C_lvl_qos_stop)
                         fill_range!(C_lvl_2.val, 0.0, C_lvl_qos, C_lvl_qos_stop)
                     end
-                    C_lvl_2.val[C_lvl_qos] = B_lvl_2_val_4
+                    C_lvl_2.val[C_lvl_qos] = A_lvl_2_val_5
                     C_lvl.idx[C_lvl_qos] = phase_stop_25
                     C_lvl_qos += 1
-                    B_lvl_q += 1
+                    A_lvl_q += 1
                 end
                 i = phase_stop_25 + 1
             end
