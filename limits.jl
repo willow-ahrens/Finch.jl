b4fe503931f48d3e10af5de99004dfc60bc229f8
Willow Ahrens <willow@csail.mit.edu>
2023-06-02 17:26:43 -0400

    add docs

--- limits.jl
+++ limits.jl
@@ -1,3 +1,45 @@
+# Author: Willow Ahrens
+# Date: 2023
+# Description: A type for representing the endpoints of closed and open intervals
+#
+
+"""
+    Limit{T}(x, s)
+
+The Limit type represents the endpoints of closed and open intervals.  The val
+field is the value of the endpoint.  The sign field is used to represent the
+openness/closedness of the interval endpoint. The sign field is 0 for closed
+intervals, Inf for open intervals, and NaN when undefined (e.g. when
+cancellation has occurred).
+
+```jl-doctest
+julia> limit(1.0)
+1.0+0
+
+julia> plus_eps(1.0)
+1.0+ϵ
+
+julia> minus_eps(1.0)
+1.0-ϵ
+
+julia> plus_eps(1.0) + minus_eps(1.0)
+1.0±ϵ
+
+julia> plus_eps(1.0) * 2
+2.0+ϵ
+
+julia> plus_eps(1.0) * minus_eps(1.0)
+1.0±ϵ
+
+julia> plus_eps(-1.0) * minus_eps(1.0)
+-1.0+ϵ
+
+julia> 1.0 < plus_eps(1.0)
+true
+
+julia> 1.0 < minus_eps(1.0)
+false
+"""
 struct Limit{T}
     val
     sign::Float32
