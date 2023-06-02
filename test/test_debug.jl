90d521fbc64de1d7c363e11c8193d7a857ac4169
teocollin1995 <teoc@mit.edu>
2023-06-02 10:42:52 -0400

    initial test file

---
+++ test/test_debug.jl
@@ -0,0 +1,34 @@
+@testset "denig" begin
+    @info "Testing Compiler Debugging"
+    function test_debug_code(code; imax=10000)
+        codes:: Vector{Any}
+        debug = Finch.stage_code(code)
+        append!(codes, debug)
+        i = 0
+        while true
+           global debug = Finch.step_code(debug, sdisplay=false)
+           append!(codes, debug)
+           global i+=1
+           if i > imax
+              return nothing
+           end
+           if Finch.iscompiled(debug)
+              break
+           end
+        end
+        ret = end_debug(debug)
+        return codes
+     end
+     
+
+    y = @fiber d(e(0.0))
+    A = @fiber d(sl(e(0.0)))
+    x = @fiber sl(e(0.0))
+
+    code1 = Finch.@finch_program_instance begin
+        @loop j i y[i] += A[i, j] * x[j]
+    end
+
+    @test check_output("debug_spmv_resume.txt", test_debug_code(code1))
+
+end
