c34a83ae028418d8362e6437e2d0cf7cc42fe513
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-06-22 10:11:55 -0400

    fix more things

--- test/reference64/concat_offset_permit.jl
+++ test/reference64/concat_offset_permit.jl
@@ -5,7 +5,7 @@ begin
     A_lvl_2 = A_lvl.lvl
     B_lvl = ((ex.bodies[2]).body.rhs.args[2]).tns.tns.lvl
     B_lvl_2 = B_lvl.lvl
-    i_stop = 10 + B_lvl.shape
+    i_stop = B_lvl.shape + -10
     C_lvl_qos_stop = 0
     resize_if_smaller!(C_lvl.ptr, 1 + 1)
     fill_range!(C_lvl.ptr, 0, 1 + 1, 1 + 1)
@@ -17,16 +17,16 @@ begin
     else
         B_lvl_i1 = 0
     end
-    phase_stop = min(i_stop, 10 + B_lvl_i1, 0)
-    if phase_stop >= 11
-        i = 11
-        if B_lvl.idx[B_lvl_q] < 11 + -(+10)
-            B_lvl_q = scansearch(B_lvl.idx, 11 + -(+10), B_lvl_q, B_lvl_q_stop - 1)
+    phase_stop = min(i_stop, -10 + B_lvl_i1, 0)
+    if phase_stop >= -9
+        i = -9
+        if B_lvl.idx[B_lvl_q] < -9 + +10
+            B_lvl_q = scansearch(B_lvl.idx, -9 + +10, B_lvl_q, B_lvl_q_stop - 1)
         end
         while i <= phase_stop
             B_lvl_i = B_lvl.idx[B_lvl_q]
-            phase_stop_2 = min(phase_stop, 10 + B_lvl_i)
-            if B_lvl_i == phase_stop_2 + -(+10)
+            phase_stop_2 = min(phase_stop, -10 + B_lvl_i)
+            if B_lvl_i == phase_stop_2 + +10
                 B_lvl_2_val_2 = B_lvl_2.val[B_lvl_q]
                 if C_lvl_qos > C_lvl_qos_stop
                     C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
@@ -42,8 +42,8 @@ begin
             i = phase_stop_2 + 1
         end
     end
-    phase_start_3 = max(1, 11)
-    phase_stop_3 = min(i_stop, 10 + B_lvl_i1, A_lvl.shape)
+    phase_start_3 = max(1, -9)
+    phase_stop_3 = min(i_stop, -10 + B_lvl_i1, A_lvl.shape)
     if phase_stop_3 >= phase_start_3
         A_lvl_q = A_lvl.ptr[1]
         A_lvl_q_stop = A_lvl.ptr[1 + 1]
@@ -58,15 +58,15 @@ begin
             if A_lvl.idx[A_lvl_q] < phase_start_3
                 A_lvl_q = scansearch(A_lvl.idx, phase_start_3, A_lvl_q, A_lvl_q_stop - 1)
             end
-            if B_lvl.idx[B_lvl_q] < phase_start_3 + -(+10)
-                B_lvl_q = scansearch(B_lvl.idx, phase_start_3 + -(+10), B_lvl_q, B_lvl_q_stop - 1)
+            if B_lvl.idx[B_lvl_q] < phase_start_3 + +10
+                B_lvl_q = scansearch(B_lvl.idx, phase_start_3 + +10, B_lvl_q, B_lvl_q_stop - 1)
             end
             while i <= phase_stop_4
                 A_lvl_i = A_lvl.idx[A_lvl_q]
                 B_lvl_i = B_lvl.idx[B_lvl_q]
-                phase_stop_5 = min(10 + B_lvl_i, phase_stop_4, A_lvl_i)
+                phase_stop_5 = min(-10 + B_lvl_i, phase_stop_4, A_lvl_i)
                 if phase_stop_5 >= i
-                    if A_lvl_i == phase_stop_5 && B_lvl_i == phase_stop_5 + -(+10)
+                    if A_lvl_i == phase_stop_5 && B_lvl_i == phase_stop_5 + +10
                         A_lvl_2_val_2 = A_lvl_2.val[A_lvl_q]
                         B_lvl_2_val_3 = B_lvl_2.val[B_lvl_q]
                         if C_lvl_qos > C_lvl_qos_stop
@@ -80,7 +80,7 @@ begin
                         C_lvl_qos += 1
                         A_lvl_q += 1
                         B_lvl_q += 1
-                    elseif B_lvl_i == phase_stop_5 + -(+10)
+                    elseif B_lvl_i == phase_stop_5 + +10
                         B_lvl_q += 1
                     elseif A_lvl_i == phase_stop_5
                         A_lvl_2_val_2 = A_lvl_2.val[A_lvl_q]
@@ -100,17 +100,17 @@ begin
             end
         end
     end
-    phase_start_7 = max(11, 1 + A_lvl.shape)
-    phase_stop_7 = min(i_stop, 10 + B_lvl_i1)
+    phase_start_7 = max(-9, 1 + A_lvl.shape)
+    phase_stop_7 = min(i_stop, -10 + B_lvl_i1)
     if phase_stop_7 >= phase_start_7
         i = phase_start_7
-        if B_lvl.idx[B_lvl_q] < phase_start_7 + -(+10)
-            B_lvl_q = scansearch(B_lvl.idx, phase_start_7 + -(+10), B_lvl_q, B_lvl_q_stop - 1)
+        if B_lvl.idx[B_lvl_q] < phase_start_7 + +10
+            B_lvl_q = scansearch(B_lvl.idx, phase_start_7 + +10, B_lvl_q, B_lvl_q_stop - 1)
         end
         while i <= phase_stop_7
             B_lvl_i = B_lvl.idx[B_lvl_q]
-            phase_stop_8 = min(10 + B_lvl_i, phase_stop_7)
-            if B_lvl_i == phase_stop_8 + -(+10)
+            phase_stop_8 = min(-10 + B_lvl_i, phase_stop_7)
+            if B_lvl_i == phase_stop_8 + +10
                 B_lvl_2_val_4 = B_lvl_2.val[B_lvl_q]
                 if C_lvl_qos > C_lvl_qos_stop
                     C_lvl_qos_stop = max(C_lvl_qos_stop << 1, 1)
@@ -126,7 +126,7 @@ begin
             i = phase_stop_8 + 1
         end
     end
-    phase_start_10 = max(1, 11, 11 + B_lvl_i1)
+    phase_start_10 = max(1, -9, -9 + B_lvl_i1)
     phase_stop_10 = min(i_stop, A_lvl.shape)
     if phase_stop_10 >= phase_start_10
         A_lvl_q = A_lvl.ptr[1]
@@ -170,5 +170,5 @@ begin
     qos = C_lvl.ptr[end] - 1
     resize!(C_lvl.idx, qos)
     resize!(C_lvl_2.val, qos)
-    (C = Fiber((SparseListLevel){Int64, Int64}(C_lvl_2, B_lvl.shape + +10, C_lvl.ptr, C_lvl.idx)),)
+    (C = Fiber((SparseListLevel){Int64, Int64}(C_lvl_2, B_lvl.shape + -(+10), C_lvl.ptr, C_lvl.idx)),)
 end
