00ac0709731f5a3b4562e7b4a461f1675a5fbfc1
Willow Ahrens <willow@csail.mit.edu>
2023-06-26 14:13:19 -0400

    a few fixes

--- src/base/limits.jl
+++ src/base/limits.jl
@@ -22,15 +22,16 @@ julia> positive_tiny() * 2
 +ϵ
 
 julia> positive_tiny() * negative_tiny()
-+0
+-ϵ
 """
 struct Infinitesimal <: Number
     sign::Int8
 end
 
 tiny(x) = Infinitesimal(x)
-tiny() = tiny(Int8(0))
-const Eps = tiny(Int8(1))
+tiny_zero() = tiny(Int8(0))
+tiny_positive() = tiny(Int8(1))
+tiny_negative() = tiny(Int8(-1))
 
 function Base.show(io::IO, x::Infinitesimal)
     print(io, "tiny(", x.sign, "ϵ)")
@@ -51,7 +52,7 @@ end
 #Core definitions for limit type
 Base.:(+)(x::Infinitesimal, y::Infinitesimal) = tiny(min(max(x.sign + y.sign, Int8(-1)), Int8(1))) # only operation that needs to be fast
 Base.:(-)(x::Infinitesimal, y::Infinitesimal) = tiny(min(max(x.sign - y.sign, Int8(-1)), Int8(1))) # only operation that needs to be fast
-Base.:(*)(x::Infinitesimal, y::Infinitesimal) = tiny(0)
+Base.:(*)(x::Infinitesimal, y::Infinitesimal) = tiny(x.sign * y.sign)
 Base.:(<)(x::Infinitesimal, y::Infinitesimal) = x.sign < y.sign
 Base.:(<=)(x::Infinitesimal, y::Infinitesimal) = x.sign <= y.sign
 Base.:(==)(x::Infinitesimal, y::Infinitesimal) = x.sign == y.sign
@@ -63,19 +64,20 @@ Base.max(x::Infinitesimal, y::Infinitesimal) = tiny(max(x.sign, y.sign))
 Base.:(+)(x::Infinitesimal) = x
 Base.:(-)(x::Infinitesimal) = tiny(-x.sign)
 
+Base.convert(::Type{Infinitesimal}, i::Number) = tiny(Int8(sign(i)))
+Base.convert(::Type{Infinitesimal}, i::Infinitesimal) = i
+
 #Crazy julia multiple dispatch stuff don't worry about it
 limit_types = [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128, BigInt, Float32, Float64]
 for S in limit_types
     @eval begin
         (::Type{$S})(i::Infinitesimal) = zero($S)
-        Base.convert(::Type{$S}, i::Infinitesimal) = zero($S)
+        @inline Base.promote_rule(::Type{Infinitesimal}, ::Type{$S}) = Infinitesimal
         Base.:(*)(x::$S, y::Infinitesimal) = tiny(Int8(sign(x)) * y.sign)
         Base.:(*)(y::Infinitesimal, x::$S) = tiny(x.sign * Int8(sign(y)))
     end
 end
 
-Base.promote_rule(::Type{Infinitesimal}, ::Type{Infinitesimal}) = Infinitesimal
-Base.convert(::Type{Infinitesimal}, i::Infinitesimal) = i
 Base.hash(x::Infinitesimal, h::UInt) = hash(typeof(x), hash(x.sign, h))
 
 """
@@ -116,12 +118,16 @@ false
 struct Limit{T} <: Number
     val
     sign::Infinitesimal
+    Limit{T}(x::T, y) where {T} = new{T}(x, y)
+    Limit{T}(x::T, y) where {T<:Limit} = error()
 end
 
 limit(x::T, s) where {T} = Limit{T}(x, s)
-plus_eps(x) = limit(x, Eps)
-minus_eps(x) = limit(x, -Eps)
-limit(x) = limit(x, 0.0)
+plus_eps(x) = limit(x, tiny_positive())
+minus_eps(x) = limit(x, tiny_negative())
+limit(x) = limit(x, tiny_zero())
+
+const Eps = plus_eps(Int8(0))
 
 function Base.show(io::IO, x::Limit)
     print(io, "limit(", x.val, x.sign, ")")
@@ -147,16 +153,13 @@ Base.max(x::Limit) = x
 Base.:(+)(x::Limit) = x
 Base.:(-)(x::Limit) = limit(-x.val, -x.sign)
 
-@inline Base.promote_rule(::Type{Limit{T}}, ::Type{Infinitesimal}) where {T} = Limit{T}
-
+Base.convert(::Type{Limit{T}}, i::Limit{S}) where {T, S} = limit(convert(T, i.val), i.sign)
 #Crazy julia multiple dispatch stuff don't worry about it
 limit_types = [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128, BigInt, Float32, Float64]
 for S in limit_types
     @eval begin
         @inline Base.promote_rule(::Type{Limit{T}}, ::Type{$S}) where {T} = Limit{promote_type(T, $S)}
-        @inline Base.promote_rule(::Type{Infinitesimal}, ::Type{$S}) = Limit{$S}
         Base.convert(::Type{Limit{T}}, i::$S) where {T} = limit(convert(T, i))
-        Base.convert(::Type{Limit{T}}, i::Infinitesimal) where {T} = limit(zero(T), i)
         Limit(i::$S) = Limit{$S}(i, tiny())
         (::Type{$S})(i::Limit{T}) where {T} = convert($S, i.val)
         Base.convert(::Type{$S}, i::Limit) = convert($S, i.val)
@@ -168,14 +171,6 @@ for S in limit_types
         Base.:(==)(x::$S, y::Limit) = limit(x) == y
         Base.isless(x::Limit, y::$S) = x < limit(y)
         Base.isless(x::$S, y::Limit) = limit(x) < y
-        Base.:(<)(x::Infinitesimal, y::$S) = limit(x) < limit(y)
-        Base.:(<)(x::$S, y::Infinitesimal) = limit(x) < limit(y)
-        Base.:(<=)(x::Infinitesimal, y::$S) = limit(x) <= limit(y)
-        Base.:(<=)(x::$S, y::Infinitesimal) = limit(x) <= limit(y)
-        Base.:(==)(x::Infinitesimal, y::$S) = limit(x) == limit(y)
-        Base.:(==)(x::$S, y::Infinitesimal) = limit(x) == limit(y)
-        Base.isless(x::Infinitesimal, y::$S) = limit(x) < limit(y)
-        Base.isless(x::$S, y::Infinitesimal) = limit(x) < limit(y)
     end
 end
 
