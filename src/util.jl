c06cbe6194a7a792c17607f69c250e3e436c0765
Willow Ahrens <willow@csail.mit.edu>
2023-07-24 12:31:10 -0400

    add a new staging fix

--- src/util.jl
+++ src/util.jl
@@ -1,3 +1,10 @@
+is_function_def(node) =
+    (@capture node :function(~args...)) ||
+    (@capture node :->(~args...)) ||
+    (@capture node (:(=))(:call(~f, ~args...), ~body)) ||
+    (@capture node (:(=))(:where(:call(~f, ~args...), ~types), ~body))
+
+has_function_def(root) = any(is_function_def, PostOrderDFS(root))
 struct All{F}
     f::F
 end
@@ -25,20 +32,59 @@ staged_defs = []
 """
     Finch.@staged
 
-This function is used internally in Finch in lieu of @generated functions.  It
+This function is used internally in Finch in lieu of @generated functions. It
 ensures the first Finch invocation runs in the latest world, and leaves hooks so
 that subsequent calls to [`Finch.refresh`](@ref) can update the world and
-invalidate old versions.
+invalidate old versions. If the body contains closures, this macro uses an
+eval and invokelatest strategy. Otherwise, it uses a generated function.
 """
 macro staged(def)
     (@capture def :function(:call(~name, ~args...), ~body)) || throw(ArgumentError("unrecognized function definition in @staged"))
-        
+
+    called = gensym(Symbol(name, :_called))
+    name_2 = gensym(Symbol(name, :_eval_invokelatest))
+    name_3 = gensym(Symbol(name, :_evaled))
+
     def = quote
+        $called::Bool = false
+
+        function $name_2($(args...))
+            println("hi")
+            global $called
+            if !$called
+                code = let ($(args...),) = ($(map((arg)->:(typeof($arg)), args)...),)
+                    $body
+                end
+                def = quote
+                    function $($(QuoteNode(name_3)))($($(map(QuoteNode, args)...)))
+                        $code
+                    end
+                end
+                ($@__MODULE__).eval(def)
+                $called = true
+            end
+            Base.invokelatest(($@__MODULE__).$name_3, $(args...))
+        end
+
         @generated function $name($(args...))
             # Taken from https://github.com/NHDaly/StagedFunctions.jl/blob/6fafbc560421f70b05e3df330b872877db0bf3ff/src/StagedFunctions.jl#L116
-            body = () -> $body
-            Core._apply_pure(body, ())
+            body_2 = () -> begin
+                code = $(body)
+                if has_function_def(macroexpand($@__MODULE__, code))
+                    :($($(name_2))($($args...)))
+                else 
+                    quote
+                        println("bye")
+                        $code
+                    end
+                end
+            end
+            Core._apply_pure(body_2, ())
+            #=quote
+                println($(QuoteNode(res)))
+            end=#
         end
+
     end
 
     return esc(quote
