989ee2762d5f735f5cbc72b2df2e612309417967
teocollin1995 <teoc@mit.edu>
2023-07-29 14:35:48 -0400

    abstract combinators.

---
+++ src/tensors/combinators/abstractCombinator.jl
@@ -0,0 +1,6 @@
+abstract type AbstractCombinator end
+abstract type AbstractVirtualCombinator end
+
+is_concurrent(lvl::AbstractVirtualCombinator, ctx, ::Union{::typeof(defaultread), ::typeof(walk), ::typeof(gallop), ::typeof(follow), typeof(defaultupdate), typeof(laminate), typeof(extrude)}, protos...) = false
+
+is_concurrent(lvl::AbstractVirtualCombinator, ctx) = false
