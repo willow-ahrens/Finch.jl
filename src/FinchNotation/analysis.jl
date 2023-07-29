fa83bb03d53f85e219a67a8244c7272eb2b3aa53
teocollin1995 <teoc@mit.edu>
2023-07-29 12:33:00 -0400

    more detail sketch of analysis; not committing level functions just yet.

--- src/FinchNotation/analysis.jl
+++ src/FinchNotation/analysis.jl
@@ -12,6 +12,69 @@ You aren't allowed to update a tensor without accessing it with i or marking ato
 new array: make_atomic
 """
 
+function gatherAcceses(prog) :: Vector{FinchNode}
+    ret:: Vector{FinchNode} = []
+    for node in PostOrderDFS(prog)
+        if @capture node access(~tns, ~mode, ~idxs...)
+            push!(ret, node)
+        else
+            continue
+        end
+    end
+    return ret
+end
+
+function gatherAssignments(prog) :: Vector{FinchNode}
+    ret:: Vector{FinchNode} = []
+    for node in PostOrderDFS(prog)
+        if @capture node assign(~lhs, ~op, ~rhs)
+            push!(ret, node)
+        else
+            continue
+        end
+    end
+    return ret
+end
+
+function gatherLocalDeclerations(prog) :: Vector{FinchNode}
+    ret:: Vector{FinchNode} = []
+    for node in PostOrderDFS(prog)
+        if @capture node declare(~tns, ~init)
+            push!(ret, tns)
+        else
+            continue
+        end
+    end
+    return ret
+end
+
+
+struct ParallelAnalysisResults
+    naive:: bool
+    withAtomics:: bool
+    withAtomicsAndAssoc:: bool
+    tensorsNeedingAtomics:: Vector{FinchNode}
+    nonAssocAssigns:: Vector{FinchNode}
+    nonConCurrentAccss::Vector{FinchNode}
+end
+
+function parallelAnalysis(prog) :: ParallelAnalysisResults
+    accs = gatherAcceses(prog)
+    assigns = gatherAssignments(prog)
+    locDefs = gatherLocalDeclerations(prog)
+
+    # Step 0:Filter out local defs
+    # Run through accs to check properties.
+
+    # Step 1: Gather all the assigns and group them per root
+    # Step 2: For each group, ensure they are all accessed via a plain i and using the same part of the tensor - (i.e the virtuals are identical) -  if not, add the root to the group needing atomics.
+    # Step 3: Similarly, for associativity
+    # Step 4: Look through all accesses and make sure they are concurrent. 
+
+    return ParallelAnalysisResults(false, false, false, [], [], [])
+end
+
+
 
 #=
 # willow says hello!
