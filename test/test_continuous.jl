64d4d4f35aa177735d6f6e6198f81da9a4af6215
nullplay <jaeyeon@mit.edu>
2023-08-08 23:40:47 -0400

    mersenne

--- test/test_continuous.jl
+++ test/test_continuous.jl
@@ -8,8 +8,8 @@
     NumItvl = 10 #100 
 
     # Generating Random Intervals
-    v = sort(sample(Random.seed!(1234), 1:Shape, NumItvl*2, replace=false))
-    w = sort(sample(Random.seed!(4321), 1:Shape, NumItvl*2, replace=false))
+    v = sort(sample(MersenneTwister(1234), 1:Shape, NumItvl*2, replace=false))
+    w = sort(sample(MersenneTwister(4321), 1:Shape, NumItvl*2, replace=false))
     s1, e1 = v[begin:2:end], v[begin+1:2:end]
     s2, e2 = w[begin:2:end], w[begin+1:2:end]
 
@@ -71,8 +71,8 @@
 
     let
         colptr = [1+NumItvl*i for i in 0:NumItvl]
-        endpoint1 = vcat([sort(sample(Random.seed!(i), 1:Shape, NumItvl*2, replace=false)) for i in 1:NumItvl]...)
-        endpoint2 = vcat([sort(sample(Random.seed!(i+NumItvl), 1:Shape, NumItvl*2, replace=false)) for i in 1:NumItvl]...)
+        endpoint1 = vcat([sort(sample(MersenneTwister(i), 1:Shape, NumItvl*2, replace=false)) for i in 1:NumItvl]...)
+        endpoint2 = vcat([sort(sample(MersenneTwister(i+NumItvl), 1:Shape, NumItvl*2, replace=false)) for i in 1:NumItvl]...)
         col_s1, col_e1 = endpoint1[begin:2:end], endpoint1[begin+1:2:end]
         col_s2, col_e2 = endpoint2[begin:2:end], endpoint2[begin+1:2:end]
 
@@ -116,7 +116,7 @@
 
     let
         colptr = [1+NumItvl*i for i in 0:NumItvl]
-        endpoint1 = vcat([sort(sample(Random.seed!(i), 1:Shape, NumItvl*2, replace=false)) for i in 1:NumItvl]...)
+        endpoint1 = vcat([sort(sample(MersenneTwister(i), 1:Shape, NumItvl*2, replace=false)) for i in 1:NumItvl]...)
         col_s1, col_e1 = endpoint1[begin:2:end], endpoint1[begin+1:2:end]
 
         x1 = Fiber(SparseList{Float32}(SparseRLE{Float32}(Element{0}(fill(1, NumItvl*NumItvl)), Shape, colptr, col_s1, col_e1), Shape, [1, NumItvl+1], e1))
