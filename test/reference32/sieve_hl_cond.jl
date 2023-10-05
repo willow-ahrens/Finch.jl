37e119ea6595c24ff9ad2e653cf1f96d1a9f9994
Robot <robot@server>
2023-10-05 14:30:48 +0000

    Regenerate test output

--- test/reference32/sieve_hl_cond.jl
+++ test/reference32/sieve_hl_cond.jl
@@ -10,24 +10,22 @@ begin
     else
         A_lvl_i1 = 0
     end
-    phase_stop = min(A_lvl_i1, A_lvl.shape)
-    if phase_stop >= 1
-        j = 1
-        if A_lvl.idx[A_lvl_q] < 1
-            A_lvl_q = Finch.scansearch(A_lvl.idx, 1, A_lvl_q, A_lvl_q_stop - 1)
+    phase_start_2 = max(1, 1 + (1 - 1))
+    phase_stop_2 = min(A_lvl.shape, A_lvl_i1, 1)
+    if phase_stop_2 >= phase_start_2
+        j = phase_start_2
+        if A_lvl.idx[A_lvl_q] < phase_start_2
+            A_lvl_q = Finch.scansearch(A_lvl.idx, phase_start_2, A_lvl_q, A_lvl_q_stop - 1)
         end
-        while j <= phase_stop
+        while j <= phase_stop_2
             A_lvl_i = A_lvl.idx[A_lvl_q]
-            phase_stop_2 = min(phase_stop, A_lvl_i)
-            if A_lvl_i == phase_stop_2
-                A_lvl_2_val_2 = A_lvl_2.val[A_lvl_q]
-                cond = phase_stop_2 == 1
-                if cond
-                    B_val = A_lvl_2_val_2 + B_val
-                end
+            phase_stop_3 = min(phase_stop_2, A_lvl_i)
+            if A_lvl_i == phase_stop_3
+                A_lvl_2_val_3 = A_lvl_2.val[A_lvl_q]
+                B_val = A_lvl_2_val_3 + B_val
                 A_lvl_q += 1
             end
-            j = phase_stop_2 + 1
+            j = phase_stop_3 + 1
         end
     end
     (B = (Scalar){0.0, Float64}(B_val),)
