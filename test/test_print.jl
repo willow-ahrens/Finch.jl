2d34fd2fb682c4d0f6b4e950fbc99bce8264ce4d
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-10-06 18:41:16 -0400

    hmm

--- test/test_print.jl
+++ test/test_print.jl
@@ -4,15 +4,15 @@
     A = fiber([(i + j) % 3 for i = 1:5, j = 1:10])
 
     formats = [
-        "list" => SparseList{Int64},
-        "byte" => SparseByteMap{Int64},
-        "hash1" => SparseHash{1, Tuple{Int64}},
-        "coo1" => SparseCOO{1, Tuple{Int64}},
+        "list" => SparseList,
+        "byte" => SparseByteMap,
+        "hash1" => SparseHash{1},
+        "coo1" => SparseCOO{1},
     ]
 
     for (rown, rowf) in formats
         @testset "print $rown d" begin
-            B = dropdefaults!(Fiber!(rowf(Dense{Int64}(Element{0.0, Float64, Int64}()))), A)
+            B = dropdefaults!(Fiber!(rowf(Dense(Element{0.0}()))), A)
             @test check_output("print_$(rown)_dense.txt", sprint(show, B))
             @test check_output("print_$(rown)_dense_small.txt", sprint(show, B, context=:compact=>true))
             @test check_output("display_$(rown)_dense.txt", sprint(show, MIME"text/plain"(), B))
@@ -22,7 +22,7 @@
 
     for (coln, colf) in formats
         @testset "print d $coln" begin
-            B = dropdefaults!(Fiber!(Dense{Int64}(colf(Element{0.0}()))), A)
+            B = dropdefaults!(Fiber!(Dense(colf(Element{0.0}()))), A)
             @test check_output("print_dense_$coln.txt", sprint(show, B))
             @test check_output("print_dense_$(coln)_small.txt", sprint(show, B, context=:compact=>true))
             @test check_output("display_dense_$(coln).txt", sprint(show, MIME"text/plain"(), B))
@@ -31,8 +31,8 @@
     end
 
     formats = [
-        "hash2" => SparseHash{2, Tuple{Int64, Int64}},
-        "coo2" =>SparseCOO{2, Tuple{Int64, Int64}},
+        "hash2" => SparseHash{2},
+        "coo2" =>SparseCOO{2},
         ]
 
     for (rowcoln, rowcolf) in formats
@@ -48,12 +48,12 @@
     A = fiber([fld(i + j, 3) for i = 1:5, j = 1:10])
 
     formats = [
-        "rle" => RepeatRLE{0.0, Int64, Int64},
+        "rle" => RepeatRLE{0.0},
     ]
 
     for (coln, colf) in formats
         @testset "print d $coln" begin
-            B = dropdefaults!(Fiber!(Dense{Int64}(colf())), A)
+            B = dropdefaults!(Fiber!(Dense(colf())), A)
             @test check_output("print_dense_$coln.txt", sprint(show, B))
             @test check_output("print_dense_$(coln)_small.txt", sprint(show, B, context=:compact=>true))
             @test check_output("display_dense_$(coln).txt", sprint(show, MIME"text/plain"(), B))
