06b933d23edb1baf6e2f5d8edfbf362c46bfd0cb
Willow Ahrens <willow.marie.ahrens@gmail.com>
2023-11-17 13:07:50 -0500

    add test coverage

--- test/test_issues.jl
+++ test/test_issues.jl
@@ -470,4 +470,23 @@ using CIndices
             end
         end)
     end
+
+    #https://github.com/willow-ahrens/Finch.jl/issues/313
+    let
+        edge_matrix = Fiber!(SparseList(SparseList(Element(0.0), 254156), 254156))
+        edge_values = fsprand((254156, 254156), .0001)
+        @finch (edge_matrix .= 0; for j=_, i=_; edge_matrix[i,j] = edge_values[i,j]; end)
+        output_matrix = Fiber!(SparseHash{1}(SparseHash{1}(Element(0.0), (254156,)), (254156,)))
+        @finch (for v_4=_, v_3=_, v_2=_, v_5=_; output_matrix[v_2,v_5] += edge_matrix[v_5, v_4]*edge_matrix[v_2, v_3]*edge_matrix[v_3, v_4]; end)
+
+        a_matrix = [1 0; 0 1]
+        a_fiber = Fiber!(SparseList(SparseList(Element(0.0), 2), 2))
+        copyto!(a_fiber, a_matrix)
+        b_matrix = [0 1; 1 0]
+        b_fiber = Fiber!(SparseList(SparseList(Element(0.0), 2), 2))
+        copyto!(b_fiber, b_matrix)
+        output_tensor = Fiber!(SparseHash{1}(SparseHash{1}(Element(0.0), (2,)), (2,)))
+
+        @finch (output_tensor .=0; for j=_,i=_,k=_; output_tensor[i,k] += a_fiber[i,j] * b_fiber[k,j]; end)
+    end
 end
