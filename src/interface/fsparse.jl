49f305fab75bb1aca2e48d7ccc067b9d0db8f87a
kylebd99 <kylebd99@gmail.com>
2024-02-27 14:00:52 -0800

    move the reverse in fsparse to the end

--- src/interface/fsparse.jl
+++ src/interface/fsparse.jl
@@ -22,9 +22,9 @@ julia> V = [1.0; 2.0; 3.0];
 
 julia> fsparse(I, V)
 SparseCOO (0.0) [1:3×1:3×1:3]
-│ │ │ 
+│ │ │
 └─└─└─[1, 1, 1] [2, 2, 2] [3, 3, 3]
-      1.0       2.0       3.0    
+      1.0       2.0       3.0
 """
 fsparse(iV::AbstractVector, args...) = fsparse_parse((), iV, args...)
 fsparse_parse(I, i::AbstractVector, args...) = fsparse_parse((I..., i), args...)
@@ -32,7 +32,7 @@ fsparse_parse(I, V::AbstractVector) = fsparse_impl(I, V)
 fsparse_parse(I, V::AbstractVector, m::Tuple) = fsparse_impl(I, V, m)
 fsparse_parse(I, V::AbstractVector, m::Tuple, combine) = fsparse_impl(I, V, m, combine)
 function fsparse_impl(I::Tuple, V::Vector, shape = map(maximum, I), combine = eltype(V) isa Bool ? (|) : (+))
-    C = map(tuple, reverse(I)...)
+    C = map(tuple, I...)
     updater = false
     if !issorted(C)
         P = sortperm(C)
@@ -50,10 +50,9 @@ function fsparse_impl(I::Tuple, V::Vector, shape = map(maximum, I), combine = el
     if updater
         I = map(i -> similar(i, length(C)), I)
         foreach(((p, c),) -> ntuple(n->I[n][p] = c[n], length(I)), enumerate(C))
-    else
-        I = map(copy, I)
     end
-    return fsparse!(I..., V, shape)
+    I = map(copy, reverse(I))
+    return fsparse!(I..., V, M)
 end
 
 """
@@ -90,13 +89,13 @@ SparseCOO (false) [1:3,1:3]
 ├─├─[3, 1]: true
 ├─├─[2, 2]: true
 ├─├─[3, 2]: true
-├─├─[3, 3]: true  
+├─├─[3, 3]: true
 
 julia> fsprand(Float64, 2, 2, 2, 0.5)
 SparseCOO (0.0) [1:2,1:2,1:2]
 ├─├─├─[2, 2, 1]: 0.6478553157718558
 ├─├─├─[1, 1, 2]: 0.996665291437684
-├─├─├─[2, 1, 2]: 0.7491940599574348 
+├─├─├─[2, 1, 2]: 0.7491940599574348
 ```
 """
 fsprand(args...) = fsprand_parse_rng(args...)
@@ -141,7 +140,7 @@ See also: (`spzeros`)(https://docs.julialang.org/en/v1/stdlib/SparseArrays/#Spar
 ```jldoctest
 julia> fspzeros(Bool, 3, 3)
 SparseCOO{2} (false) [:,1:3]
-    
+
 julia> fspzeros(Float64, 2, 2, 2)
 SparseCOO{3} (0.0) [:,:,1:2]
 ```
