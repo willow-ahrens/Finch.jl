cb0be3fac494d157c8c49da7e82c84fc7897afc0
kylebd99 <kylebd99@gmail.com>
2024-02-28 11:10:38 -0800

    add function interface

--- src/interface/einsum.jl
+++ src/interface/einsum.jl
@@ -39,3 +39,44 @@ macro einsum_prgm(expr)
     println(einsum_parser(expr))
     return nothing
 end
+
+struct EinsumDef
+    inputs
+    input_indices
+    output_symbol
+    output_indices
+    pointwise_op
+    reduce_op
+    is_lazy
+end
+
+function einsum_arg(t, idxs...)
+    input_indices = [Symbol[]]
+    for idx in idxs
+        if idx isa Symbol
+            push!(input_indices[1], idx)
+        elseif idx isa String
+            push!(input_indices[1], Symbol(idx))
+        else
+            FinchSyntaxError("Einsum arguments must be indexed by symbols or strings.")
+        end
+    end
+    is_lazy = t isa LogicTensor
+    return EinsumDef([t], input_indices, nothing, nothing, nothing, nothing, is_lazy)
+end
+
+function einsum_op(pointwise_op, args...)
+    inputs = [arg.inputs[1] for arg in args]
+    input_indices = [arg.input_indices[1] for arg in args]
+    is_lazy = any([arg.is_lazy for arg in args])
+    return EinsumDef(inputs, input_indices, nothing, nothing, pointwise_op, nothing, is_lazy)
+end
+
+function einsum_reduce(reduce_op, output, output_indices, e_op)
+    return EinsumDef(e_op.inputs, e_op.input_indices, output, output_indices, e_op.pointwise_op, reduce_op, e_op.is_lazy)
+end
+
+# Should return a FinchLogic statement which
+function einsum_materialize(e::EinsumDef)
+    return nothing
+end
