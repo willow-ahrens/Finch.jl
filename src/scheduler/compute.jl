fe5022ac7fc07683232d78293b752d426311f98a
Willow Ahrens <willow@csail.mit.edu>
2024-05-04 22:10:31 -0400

    cleaned up interpreter

--- src/scheduler/compute.jl
+++ src/scheduler/compute.jl
@@ -1,33 +1,28 @@
-struct DefaultOptimizer
+struct DefaultLogicOptimizer
     ctx
 end
 
-default_optimizer = DefaultOptimizer(FinchCompiler())
+function (ctx::DefaultLogicOptimizer)(prgm)
+    prgm = optimize(prgm)
+    ctx.ctx(prgm)
+end
+
+default_scheduler = DefaultLogicOptimizer(FinchCompiler())
 
 """
-    compute(args..., ctx=default_optimizer) -> Any
+    compute(args..., ctx=default_scheduler) -> Any
 
 Compute the value of a lazy tensor. The result is the argument itself, or a
 tuple of arguments if multiple arguments are passed.
 """
-compute(args...; ctx=default_optimizer) = compute_parse(args, ctx)
-compute(arg; ctx=default_optimizer) = compute_parse((arg,), ctx)[1]
-compute(args::Tuple; ctx=default_optimizer) = compute_parse(args, ctx)
-function compute_parse(args::Tuple, ctx)
+compute(args...; ctx=default_scheduler) = compute_parse(ctx, args)
+compute(arg; ctx=default_scheduler) = compute_parse(ctx, (arg,))[1]
+compute(args::Tuple; ctx=default_scheduler) = compute_parse(ctx, args)
+function compute_parse(ctx, args::Tuple)
     args = collect(args)
     vars = map(arg -> alias(gensym(:A)), args)
     bodies = map((arg, var) -> query(var, arg.data), args, vars)
     prgm = plan(bodies, produces(vars))
 
-    return compute_impl(prgm, ctx)
-end
-
-function compute_impl(prgm, ctx::DefaultOptimizer)
-    compute_impl(prgm, ctx.ctx)
-end
-
-function compute_impl(prgm, ctx::FinchInterpreter)
-    prgm = optimize(prgm)
-    prgm = format_queries(prgm)
-    ctx(prgm)
+    return ctx(prgm)
 end
