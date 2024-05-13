16ee6b71d613b12677e9ac89dd753542b3554e68
Willow Ahrens <willow@csail.mit.edu>
2024-05-13 00:24:06 -0400

    logging

--- test/precompile.jl
+++ test/precompile.jl
@@ -1,7 +1,7 @@
 let
     #This is a script intended to exercise the basic functionality of Finch during precompilation so that Finch will load faster.
 
-    #the key formats will be 0, 1, 2, and 3 dimensional tensors. The key formats will be Dense, Sparse, CSF, and COO
+    #the key formats will be 0, 1, 2, and 3 dimensional tensors. The key formats will be Dense, DCSF, CSF, and COO
 
     #We will test the finch macro through the high-level interface.
 
@@ -10,101 +10,95 @@ let
     #We will use Bool, Int, and Float64, and default values will be 0 and 1
 
     formats = []
-    for T in [Bool, Int, Float32, Float64]
-        for F in [0, 1]
-            f = T(F)
-            for N in 0:3
-                vals = rand(T, [2 for _ in 1:N]...)
-                if N == 0
-                    push!(formats, Scalar(f))
-                else
-                    bases = []
-                    push!(bases, Element(f))
-                    if T == Bool && F == 0
-                        push!(bases, Pattern())
-                    end
-                    for base in bases
+    Ts = [Bool, Int, Float32, Float64]
+    
+    tik = time()
+    for (n, T) in enumerate(Ts)
+        if n > 1
+            tok = time()
+            estimated = ceil(Int, (tok - tik)/(n - 1) * (length(Ts) - n + 1))
+            @info "Precompiling common tensor formats... (estimated: $(fld(estimated, 60)) minutes and $(mod(estimated, 60)) seconds)"
+        else
+            @info "Precompiling common tensor formats..."
+        end
+        f = zero(T)
+        for N in 0:3
+            vals = rand(T, [2 for _ in 1:N]...)
+            if N == 0
+                push!(formats, Scalar(f))
+            else
+                bases = []
+                push!(bases, Element(f))
+                if T == Bool
+                    push!(bases, Pattern())
+                end
+                for base in bases
+                    if !(base isa Pattern)
                         #Dense
                         format = deepcopy(base)
                         for i in 1:N
                             format = Dense(format)
                         end
                         push!(formats, Tensor(format, vals))
+                    end
 
-                        #Sparse
-                        format = deepcopy(base)
-                        for i in 1:N
-                            format = Sparse(format)
-                        end
-                        push!(formats, Tensor(format, vals))
+                    #DCSF
+                    format = deepcopy(base)
+                    for i in 1:N
+                        format = SparseList(format)
+                    end
+                    push!(formats, Tensor(format, vals))
 
+                    if N >= 1
                         #CSF
                         if N >= 2
                             format = deepcopy(base)
                             for i in 1:(N - 1)
-                                format = Sparse(format)
+                                format = SparseList(format)
                             end
                             format = Dense(format)
                             push!(formats, Tensor(format, vals))
                         end
-
-                        #COO
-                        push!(formats, Tensor(SparseCOO{N}(deepcopy(base)), vals))
                     end
                 end
             end
         end
     end
 
-    for format in formats
-        As = Any[deepcopy(format)]
-        Bs = Any[deepcopy(format)]
-        if ndims(format) >= 2
-            push!(As, swizzle(deepcopy(format), (ndims(format):-1:1)...))
-            push!(Bs, swizzle(deepcopy(format), (ndims(format):-1:1)...))
+    for (n, format) in enumerate(formats)
+        if n > 1
+            tok = time()
+            estimated = ceil(Int, (tok - tik)/(n - 1) * (length(formats) - n + 1))
+            @info "Precompiling common tensor operations... (estimated: $(fld(estimated, 60)) minutes and $(mod(estimated, 60)) seconds)"
+        else
+            @info "Precompiling common tensor operations..."
         end
+        A = deepcopy(format)
+        B = deepcopy(format)
 
-        for A = As
-            if ndims(format) > 0
-                dropdefaults(A)
-            end
-            i = rand(1:2, ndims(A))
-            A[i...]
-            if ndims(format) == 2
-                x = rand(eltype(format), size(A)[2])
-                @einsum y[i] += A[i, j] * x[j]
-            end
-            if eltype(format) == Bool
-                all(A)
-                any(A)
-                .!(A)
-            end
-            if eltype(format) <: Union{Integer, AbstractFloat} && eltype(format) != Bool
-                sum(A)
-                prod(A)
-                maximum(A)
-                minimum(A)
-                extrema(A)
-                norm(A)
-            end
+        if ndims(format) > 0
+            dropdefaults(A)
         end
-        for A = As, B = Bs
-            copyto!(A, B)
-            if eltype(format) <: Integer
-                A .& B
-                A .| B
-                A .⊻ B
-            end
-            if eltype(format) <: Union{Integer, AbstractFloat} && eltype(format) != Bool
-                A .+ B
-                A .- B
-                A .* B
-                max.(A, B)
-                min.(A, B)
-                if ndims(format) == 2
-                    @einsum C[i, j] += A[i, k] * B[k, j]
-                end
-            end
+        copyto!(A, B)
+        A == B
+        i = rand(1:2, ndims(A))
+        A[i...]
+        if eltype(format) == Bool
+            .!(A)
+            any(A)
+            all(A)
+        end
+        if eltype(format) <: Integer
+            .~(A)
+            A .& B
+            A .| B
+        end
+        if eltype(format) <: Union{Integer, AbstractFloat} && eltype(format) != Bool
+            sum(A)
+            A .+ B
+            A .- B
+            A .* B
         end
     end
+    @info "Done!"
 end
