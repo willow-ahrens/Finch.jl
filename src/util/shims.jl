0241bc61a1e42e8646aa2b321bcf69092ae9daed
kylebd99 <kylebd99@gmail.com>
2024-05-22 16:11:27 -0700

    Simple exponential search implementation

--- src/util/shims.jl
+++ src/util/shims.jl
@@ -15,16 +15,21 @@ end
     scansearch(v, x, lo, hi)
 
 return the first value of `v` greater than or equal to `x`, within the range
-`lo:hi`. Return `hi+1` if all values are less than `x`.
+`lo:hi`. Return `hi+1` if all values are less than `x`. This implemantation uses an
+exponential search strategy which involves two steps: 1) searching for binary search bounds
+via exponential steps rightward 2) binary searching within those bounds.
 """
 Base.@propagate_inbounds function scansearch(v, x, lo::T1, hi::T2) where {T1<:Integer, T2<:Integer} # TODO types for `lo` and `hi` #406
     u = T1(1)
-    stop = min(hi, lo + T1(32))
-    while lo + u < stop && v[lo] < x
-        lo += u
+    d = u
+    p = lo
+    while p < hi && v[p] < x
+        d *= T1(2)
+        p += d
     end
-    lo = lo - u
-    hi = hi + u
+    lo = p - d - u
+    hi = min(p, hi) + u
+
     while lo < hi - u
         m = lo + ((hi - lo) >>> 0x01)
         if v[m] < x
