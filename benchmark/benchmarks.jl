a68259baab124e54e42c678ab3a04e6bdcd00336
Willow Ahrens <willow.marie.ahrens@gmail.com>
2024-06-12 17:56:59 -0400

    cleanup

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,9 +9,6 @@ using Finch
 using BenchmarkTools
 using MatrixDepot
 using SparseArrays
-using FastClosures
-using Polyester
-
 include(joinpath(@__DIR__, "../docs/examples/bfs.jl"))
 include(joinpath(@__DIR__, "../docs/examples/pagerank.jl"))
 include(joinpath(@__DIR__, "../docs/examples/shortest_paths.jl"))
@@ -224,7 +221,6 @@ end
 
 function spmv_threaded(A, x)
     y = Tensor(Dense{Int64}(Element{0.0, Float64}()))
-    #=
     @finch begin
         y .= 0
         for i=parallel(_)
@@ -234,80 +230,6 @@ function spmv_threaded(A, x)
         end
         return y
     end
-    =#
-    spmv_threaded_helper(y, A, x)
-end
-
-function spmv_threaded_helper(y::Tensor{DenseLevel{Int64, ElementLevel{0.0, Float64, Int64, Vector{Float64}}}}, A::Tensor{DenseLevel{Int64, SparseListLevel{Int64, Vector{Int64}, Vector{Int64}, ElementLevel{0.0, Float64, Int64, Vector{Float64}}}}}, x::Tensor{DenseLevel{Int64, ElementLevel{0.0, Float64, Int64, Vector{Float64}}}})
-    @inbounds @fastmath(begin
-        y_lvl = y.lvl
-        y_lvl_2 = y_lvl.lvl
-        y_lvl_val = y_lvl.lvl.val
-        A_lvl = A.lvl
-        A_lvl_2 = A_lvl.lvl
-        A_lvl_ptr = A_lvl_2.ptr
-        A_lvl_idx = A_lvl_2.idx
-        A_lvl_2_val = A_lvl_2.lvl.val
-        x_lvl = x.lvl
-        x_lvl_val = x_lvl.lvl.val
-        x_lvl.shape == A_lvl_2.shape || throw(DimensionMismatch("mismatched dimension limits ($(x_lvl.shape) != $(A_lvl_2.shape))"))
-        Finch.resize_if_smaller!(y_lvl_val, A_lvl.shape)
-        Finch.fill_range!(y_lvl_val, 0.0, 1, A_lvl.shape)
-        val = y_lvl_val
-        y_lvl_val = (Finch).moveto(y_lvl_val, CPU(Threads.nthreads()))
-        x_lvl_val = (Finch).moveto(x_lvl_val, CPU(Threads.nthreads()))
-        A_lvl_ptr = (Finch).moveto(A_lvl_ptr, CPU(Threads.nthreads()))
-        A_lvl_idx = (Finch).moveto(A_lvl_idx, CPU(Threads.nthreads()))
-        A_lvl_2_val = (Finch).moveto(A_lvl_2_val, CPU(Threads.nthreads()))
-        f = @closure (i_4) -> @inbounds begin
-            phase_start_2 = max(1, 1 + fld(A_lvl.shape * (i_4 + -1), Threads.nthreads()))
-            phase_stop_2 = min(A_lvl.shape, fld(A_lvl.shape * i_4, Threads.nthreads()))
-            if phase_stop_2 >= phase_start_2
-                for i_7 = phase_start_2:phase_stop_2
-                    y_lvl_q = (1 - 1) * A_lvl.shape + i_7
-                    A_lvl_q = (1 - 1) * A_lvl.shape + i_7
-                    A_lvl_2_q = A_lvl_ptr[A_lvl_q]
-                    A_lvl_2_q_stop = A_lvl_ptr[A_lvl_q + 1]
-                    if A_lvl_2_q < A_lvl_2_q_stop
-                        A_lvl_2_i1 = A_lvl_idx[A_lvl_2_q_stop - 1]
-                    else
-                        A_lvl_2_i1 = 0
-                    end
-                    phase_stop_3 = min(x_lvl.shape, A_lvl_2_i1)
-                    if phase_stop_3 >= 1
-                        if A_lvl_idx[A_lvl_2_q] < 1
-                            A_lvl_2_q = Finch.scansearch(A_lvl_idx, 1, A_lvl_2_q, A_lvl_2_q_stop - 1)
-                        end
-                        while true
-                            A_lvl_2_i = A_lvl_idx[A_lvl_2_q]
-                            if A_lvl_2_i < phase_stop_3
-                                A_lvl_3_val = A_lvl_2_val[A_lvl_2_q]
-                                x_lvl_q = (1 - 1) * x_lvl.shape + A_lvl_2_i
-                                x_lvl_2_val = x_lvl_val[x_lvl_q]
-                                y_lvl_val[y_lvl_q] += A_lvl_3_val * x_lvl_2_val
-                                A_lvl_2_q += 1
-                            else
-                                phase_stop_5 = min(phase_stop_3, A_lvl_2_i)
-                                if A_lvl_2_i == phase_stop_5
-                                    A_lvl_3_val = A_lvl_2_val[A_lvl_2_q]
-                                    x_lvl_q = (1 - 1) * x_lvl.shape + phase_stop_5
-                                    x_lvl_2_val_2 = x_lvl_val[x_lvl_q]
-                                    y_lvl_val[y_lvl_q] += A_lvl_3_val * x_lvl_2_val_2
-                                    A_lvl_2_q += 1
-                                end
-                                break
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        Threads.@threads for i_5 = 1:Threads.nthreads()
-            f(i_5)
-        end
-        resize!(val, A_lvl.shape)
-        (y = Tensor((DenseLevel){Int64}(y_lvl_2, A_lvl.shape)),)
-    end)
 end
 
 SUITE["parallel"]["SpMV_serial"] = BenchmarkGroup()
