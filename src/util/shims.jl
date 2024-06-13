d8497eb9b29dcd9b6e83d4231758bc18ae4ac9c5
Willow Ahrens <willow@csail.mit.edu>
2024-06-13 13:37:41 -0400

    fix

--- src/util/shims.jl
+++ src/util/shims.jl
@@ -75,6 +75,14 @@ macro barrier(args_ex...)
     end)
 end
 
+# wrap_closure is taken from https://github.com/c42f/FastClosures.jl
+#
+# The FastClosures.jl package is licensed under the MIT "Expat" License:
+# Copyright (c) 2017: Claire Foster.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 # Wrap `closure_expression` in a `let` block to improve efficiency.
 function wrap_closure(module_, ex)
     bound_vars = Symbol[]
